edd53eba2206be1287561f1dc505f5aee2d53a29
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-10-28 18:49:49 +0100

    Updated mock dataset.

--- phy/datasets/tests/test_mock.py
+++ phy/datasets/tests/test_mock.py
@@ -2,21 +2,38 @@
 
 """Tests of mock datasets."""
 
+#------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------
+
 import numpy as np
 
-from ..mock import artificial_waveforms
+from ..mock import artificial_waveforms, artificial_traces
 
 
-def test_artificial(plot=False):
+#------------------------------------------------------------------------------
+# Tests
+#------------------------------------------------------------------------------
+
+def _plot(arr):
+    import matplotlib.pyplot as plt
+    plt.plot(arr)
+    plt.show()
+
+
+def test_artificial():
     nspikes = 10
-    nsamples = 32
+    nsamples_waveforms = 32
+    nsamples_traces = 1000
     nchannels = 64
 
+    # Waveforms.
     waveforms = artificial_waveforms(nspikes=nspikes,
-                                     nsamples=nsamples,
+                                     nsamples=nsamples_waveforms,
                                      nchannels=nchannels)
-    assert waveforms.shape == (nspikes, nsamples, nchannels)
+    assert waveforms.shape == (nspikes, nsamples_waveforms, nchannels)
 
-    # import matplotlib.pyplot as plt
-    # plt.plot(waveforms[..., 0].T)
-    # plt.show()
+    # Traces.
+    traces = artificial_traces(nsamples=nsamples_traces,
+                               nchannels=nchannels)
+    assert traces.shape == (nsamples_traces, nchannels)
