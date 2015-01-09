8136af41aa888bf1ac04f6aa8e7fa5e4b09ef834
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-01-09 11:10:59 +0100

    Created csr_matrix().

--- phy/io/sparse.py
+++ phy/io/sparse.py
@@ -15,5 +15,28 @@ from ..ext import six
 # Sparse CSR
 #------------------------------------------------------------------------------
 
+
+def _csr_from_dense(dense):
+    """Create a CSR structure from a dense NumPy array."""
+    raise NotImplementedError(("Creating CSR from dense matrix is not "
+                               "implemented yet."))
+
+
 class SparseCSR(object):
-    pass
+    """Sparse CSR matrix data structure."""
+    def __init__(self, data=None, channels=None, spikes_ptr=None):
+        self._data = data
+        self._channels = channels
+        self._spikes_ptr = spikes_ptr
+
+
+def csr_matrix(dense=None, data=None, channels=None, spikes_ptr=None):
+    """Create a CSR matrix from a dense matrix, or from sparse data."""
+    if dense is not None:
+        # Ensure 'dense' is a ndarray.
+        if not isinstance(dense, np.ndarray):
+            dense = np.array(dense)
+        return _csr_from_dense(dense)
+    if data is None or channels is None or spikes_ptr is None:
+        raise ValueError("data, channels, and spikes_ptr must be specified.")
+    return SparseCSR(data=data, channels=channels, spikes_ptr=spikes_ptr)
--- phy/io/tests/test_sparse.py
+++ phy/io/tests/test_sparse.py
@@ -31,4 +31,10 @@ def teardown():
 #------------------------------------------------------------------------------
 
 def test_sparse_csr():
-    pass
+    arr = np.zeros((4, 5))
+    arr[0, 1] = 1
+    arr[0, 2] = 2
+    arr[1, 0] = 3
+    arr[1, 3] = 4
+    arr[3, 4] = 5
+    arr_csr = SparseCSR(arr)
