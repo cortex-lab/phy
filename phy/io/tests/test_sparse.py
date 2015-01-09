bbc698abc612d7f60ffa88966c57af31ec0afbb1
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-01-09 19:17:12 +0100

    Working on tests.

--- phy/io/tests/test_sparse.py
+++ phy/io/tests/test_sparse.py
@@ -32,24 +32,44 @@ def teardown():
 
 
 def _dense_matrix_example():
+    """Sparse matrix example:
+
+        *   1   2   *   *
+        3   *   *   4   *
+        *   *   *   *   *
+        *   *   5   *   *
+
+    Return a dense array.
+
+    """
     arr = np.zeros((4, 5))
     arr[0, 1] = 1
     arr[0, 2] = 2
     arr[1, 0] = 3
     arr[1, 3] = 4
-    arr[3, 4] = 5
+    arr[3, 2] = 5
     return arr
 
 
+def _sparse_matrix_example():
+    """Return a sparse representation of the sparse matrix example."""
+    data = np.arange(1, 6)
+    channels = np.array([1, 2, 0, 3, 2])
+    spikes_ptr = np.array([0, 2, 4, 5])
+    return data, channels, spikes_ptr
+
+
 def test_sparse_csr_check():
+    """Test the checks performed when creating a sparse matrix."""
     arr = _dense_matrix_example()
     with raises(NotImplementedError):
         csr_matrix(arr)
 
     data_exp = np.arange(1, 6)
 
-    data = data_exp
-    channels = None
-    spikes_ptr = None
     with raises(ValueError):
-        csr_matrix(data=data, channels=channels, spikes_ptr=spikes_ptr)
+        csr_matrix(data=data_exp)
+    with raises(ValueError):
+        csr_matrix(channels=None)
+    with raises(ValueError):
+        csr_matrix(data=data_exp)
