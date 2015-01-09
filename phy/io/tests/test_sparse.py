6985fe26914aad1615d74ced14f8f3bded12282c
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-01-09 11:46:39 +0100

    WIP: tests.

--- phy/io/tests/test_sparse.py
+++ phy/io/tests/test_sparse.py
@@ -30,12 +30,24 @@ def teardown():
 # Tests
 #------------------------------------------------------------------------------
 
-def test_sparse_csr():
+
+def _dense_matrix_example():
     arr = np.zeros((4, 5))
     arr[0, 1] = 1
     arr[0, 2] = 2
     arr[1, 0] = 3
     arr[1, 3] = 4
     arr[3, 4] = 5
+    return arr
 
+
+def test_sparse_csr_check():
+    arr = _dense_matrix_example()
     assert_raises(NotImplementedError, csr_matrix, (arr,))
+
+    data_exp = np.arange(1, 6)
+
+    data = data_exp
+    channels = None
+    spikes_ptr = None
+    csr_matrix(data=data, channels=channels, spikes_ptr=spikes_ptr)
