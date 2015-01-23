Fixed normalization.

phy/plot/tests/test_waveforms.py
@@ -11,6 +11,7 @@ from vispy import app
 
 from ...utils.logging import set_level
 from ..waveforms import Waveforms, WaveformView
+from ...utils.array import _normalize
 
 
 #------------------------------------------------------------------------------
@@ -65,10 +66,8 @@ def test_waveforms():
                                   (5, 10),
                                   (0, 0)], dtype=np.float32)
 
-    # TODO: refactor this
-    channel_positions -= channel_positions.min(axis=0)
-    channel_positions /= channel_positions.max(axis=0)
-    channel_positions = .2 + .6 * channel_positions
+    channel_positions = _normalize(channel_positions)
+    print(channel_positions)
 
     n_clusters = 2
     n_channels = 32

phy/utils/array.py
@@ -24,9 +24,9 @@ def _unique(x):
 
 
 def _normalize(positions):
-    """Normalize an array into [-1, 1]."""
+    """Normalize an array into [0, 1]."""
     # TODO: add 'keep_ratio' option.
-    min, max = positions.min(), positions.max()
-    positions_n = (positions - min) / float(max - min)
-    positions_n = -1. + 2. * positions_n
+    positions = positions.astype(np.float32)
+    min, max = positions.min(axis=0), positions.max(axis=0)
+    positions_n = (positions - min) / (max - min)
     return positions_n