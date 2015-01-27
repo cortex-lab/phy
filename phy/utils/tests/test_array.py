c9c534965497073184cdd27b0fda907ee0aacfd2
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-01-27 11:37:07 +0100

    Added chunking functions.

--- phy/utils/tests/test_array.py
+++ phy/utils/tests/test_array.py
@@ -10,12 +10,12 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from pytest import raises
 
-from ..array import _unique, _normalize
+from ..array import _unique, _normalize, chunk_bounds, excerpts, data_chunk
 # from ...datasets.mock import artificial_spike_clusters
 
 
 #------------------------------------------------------------------------------
-# Tests
+# Test utility functions
 #------------------------------------------------------------------------------
 
 def test_unique():
@@ -38,3 +38,50 @@ def test_normalize():
     positions_n = _normalize(positions)
     assert positions_n.min() >= -1
     assert positions_n.max() <= 1
+
+
+#------------------------------------------------------------------------------
+# Test chunking
+#------------------------------------------------------------------------------
+
+def test_chunk_bounds():
+    chunks = chunk_bounds(200, 100, overlap=20)
+
+    assert next(chunks) == (0, 100, 0, 90)
+    assert next(chunks) == (80, 180, 90, 170)
+    assert next(chunks) == (160, 200, 170, 200)
+
+
+def test_chunk():
+    data = np.random.randn(200, 4)
+    chunks = chunk_bounds(data.shape[0], 100, overlap=20)
+
+    # Chunk 1.
+    ch = next(chunks)
+    d = data_chunk(data, ch)
+    d_o = data_chunk(data, ch, with_overlap=True)
+
+    assert np.array_equal(d_o, data[0:100])
+    assert np.array_equal(d, data[0:90])
+
+    # Chunk 2.
+    ch = next(chunks)
+    d = data_chunk(data, ch)
+    d_o = data_chunk(data, ch, with_overlap=True)
+
+    assert np.array_equal(d_o, data[80:180])
+    assert np.array_equal(d, data[90:170])
+
+
+def test_excerpts_1():
+    bounds = [(start, end) for (start, end) in excerpts(100,
+                                                        n_excerpts=3,
+                                                        excerpt_size=10)]
+    assert bounds == [(0, 10), (45, 55), (90, 100)]
+
+
+def test_excerpts_2():
+    bounds = [(start, end) for (start, end) in excerpts(10,
+                                                        n_excerpts=3,
+                                                        excerpt_size=10)]
+    assert bounds == [(0, 10)]
