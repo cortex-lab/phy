04cc7fe79c21dec65176c274b0462e2e6a977adc
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-01-28 20:46:49 +0100

    Added BaseExperiment tests.

--- phy/io/tests/test_experiment.py
+++ phy/io/tests/test_experiment.py
@@ -6,14 +6,36 @@
 # Imports
 #------------------------------------------------------------------------------
 
+from pytest import raises
 import numpy as np
 
-from pytest import raises
+from ..experiment import BaseExperiment
 
 
 #------------------------------------------------------------------------------
 # Tests
 #------------------------------------------------------------------------------
 
-def test_experiment():
-    pass
+def test_base_experiment():
+    exp = BaseExperiment()
+
+    with raises(NotImplementedError):
+        exp.metadata()
+    with raises(NotImplementedError):
+        exp.traces()
+    with raises(NotImplementedError):
+        exp.spike_times()
+    with raises(NotImplementedError):
+        exp.spike_clusters()
+    with raises(NotImplementedError):
+        exp.cluster_metadata()
+    with raises(NotImplementedError):
+        exp.features()
+    with raises(NotImplementedError):
+        exp.masks()
+    with raises(NotImplementedError):
+        exp.waveforms()
+    with raises(NotImplementedError):
+        exp.probe()
+    with raises(NotImplementedError):
+        exp.save()
