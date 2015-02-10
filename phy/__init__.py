b15b2be6bd6d064e432e7b95caa48a01fb4231b5
Cedric GESTES <cedric.gestes@gmail.com>
2015-02-10 19:14:34 +0100

    phy: code to autoinstall the nbextension.

--- phy/__init__.py
+++ phy/__init__.py
@@ -7,3 +7,14 @@ __version__ = '0.1.0-alpha'
 
 # Set up the default logger.
 default_logger()
+
+
+def prepare_js():
+    """ This is needed to map js/css to the nbextensions folder
+    """
+    from IPython.html import nbextensions
+    import os
+    pkgdir = os.path.dirname(__file__)
+    nbextensions.install_nbextension(pkgdir, symlink=True, user=True, destination='phy')
+
+#prepare_js()
