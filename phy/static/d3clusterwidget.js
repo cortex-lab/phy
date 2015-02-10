8b1c15b7eaa10fc7974b17f87d3edda839ca434a
Cedric GESTES <cedric.gestes@gmail.com>
2015-02-10 21:23:34 +0100

    d3clusterwidget: make it mostly work

--- phy/static/d3clusterwidget.js
+++ phy/static/d3clusterwidget.js
@@ -1,6 +1,7 @@
 
-// define(function(require) {
-//     d3 = require('/nbextensions/phy/static/d3.js');
+define(function(require) {
+
+    d3 = require('/nbextensions/phy/static/d3.js');
 
     function D3ClusterWidget(clusterView) {
         this.view = clusterView;
@@ -11,34 +12,31 @@
         {
             var barWidth = 1;
 
-            //var clusters = generate_all_clusters();
-
             var clusterView = d3.select(this.view);
 
-            console.log(clusterView);
-
-            clusterView.data(clusters, function(d) { return d.id; });
-
+            var cluster = clusterView.selectAll(".cluster").data(clusters, function(d) { return d.id; });
+            var that = this;
             var clusterEnter = cluster.enter()
                                 .insert("div")
                                 .attr("class", function (d) {return (d.selected) ? "cluster sel" : "cluster";})
                                 .style("fill-opacity", 0)
                                 .on('mousedown', function(d) {
-                                    clusterClicked(d, this);
+                                    that.clusterClicked(d, this);
+                                    that.redraw(clusters);
                                 });
 
             var lineFunction = d3.svg.line()
-                                 .x(function(d, i) { return this.width-(i*barWidth); })
-                                 .y(function(d, i) { return this.height-d; })
+                                 .x(function(d, i) { return that.width-(i*barWidth); })
+                                 .y(function(d, i) { return that.height-d; })
                                  .interpolate("step-after");
 
             var svgContainer = clusterEnter.append("svg")
-                                .attr("width", this.width)
-                                .attr("height", this.height);
+                                .attr("width", that.width)
+                                .attr("height", that.height);
 
 
             svgContainer.append("path")
-                        .datum(function(d) { barWidth = width/d.ccg.length; return d.ccg;})
+                        .datum(function(d) { barWidth = that.width / d.ccg.length; return d.ccg;})
                         .attr("d", function(d) {return lineFunction(d)})
                         .attr("stroke", "black")
                         .attr("stroke-width", 0)
@@ -50,57 +48,59 @@
             clusterEnter.append("div")
                 .text(function(d) { return d.nchannels + "ch"; });
 
-//
-// Clusters to update
-//
+            //
+            // Clusters to update
+            //
+            var clusterUpdate = d3.transition(cluster)
+                .style("fill-opacity", 1)
+                .attr("class", function (d) {return (d.selected) ? "cluster sel" : "cluster";});
 
-    var clusterUpdate = d3.transition(cluster)
-      .style("fill-opacity", 1)
-      .attr("class", function (d) {return (d.selected) ? "cluster sel" : "cluster";});
-//
-// Clusters to remove
-//
-
-    var clusterExit = d3.transition(cluster.exit())
-        .style("fill-opacity", 0)
-        .remove();
+            //
+            // Clusters to remove
+            //
+            var clusterExit = d3.transition(cluster.exit())
+                .style("fill-opacity", 0)
+                .remove();
         }
-    }
 
-    this.clusterClicked = function(d, node) {
 
-    cls = d3.select(this.view).selectAll('.cluster')
+        this.clusterClicked = function(d, node) {
+            console.log("clusterCliii", d, node);
+            cls = d3.select(this.view).selectAll('.cluster');
 
-    // first, deal with selection ranges
-    if (d3.event.shiftKey) {
-        var firstSelectedIndex, lastSelectedIndex, currentIndex;
+            // first, deal with selection ranges
+            if (d3.event.shiftKey) {
+                var firstSelectedIndex, lastSelectedIndex, currentIndex;
 
-            cls.each(function(dl, i) {
-            if (dl.selected) {
-              firstSelectedIndex || (firstSelectedIndex = i);
-              lastSelectedIndex = i;
+                cls.each(function(dl, i) {
+                    if (dl.selected) {
+                        firstSelectedIndex || (firstSelectedIndex = i);
+                        lastSelectedIndex = i;
+                    }
+                    if (this === node) currentIndex = i;
+                });
+                var min = Math.min(firstSelectedIndex, lastSelectedIndex, currentIndex);
+                var max = Math.max(firstSelectedIndex, lastSelectedIndex, currentIndex);
+
+                // select all between first and last selected
+                // when clicked inside a selection
+                cls.each(function(d, i) {
+
+                    // preserve state for additive selection
+                    d.selected = ((d3.event.ctrlKey || d3.event.metaKey) && d._selected) || (i >= min && i <= max);
+                });
             }
-            if (this === node) currentIndex = i;
-          });
-          var min = Math.min(firstSelectedIndex, lastSelectedIndex, currentIndex);
-          var max = Math.max(firstSelectedIndex, lastSelectedIndex, currentIndex);
-
-          // select all between first and last selected
-          // when clicked inside a selection
-          cls.each(function(d, i) {
-
-            // preserve state for additive selection
-            d.selected = ((d3.event.ctrlKey || d3.event.metaKey) && d._selected) || (i >= min && i <= max);
-          });
+            else
+            {
+                // additive select with `ctrl` key
+                if (!(d3.event.ctrlKey || d3.event.metaKey)) {
+                    cls.each(function(d) { d.selected = false; });
+                }
+                d.selected = !d.selected;
+            }
         }
-    else
-    {
-      // additive select with `ctrl` key
-      if (!(d3.event.ctrlKey || d3.event.metaKey)) {
-        cls.each(function(d) { d.selected = false; });
-      }
-      d.selected = !d.selected;
-    }
-}
-//     return { 'D3ClusterWidget' : D3ClusterWidget };
-// });
+
+    } // end class D3ClusterWidget
+
+    return { 'D3ClusterWidget' : D3ClusterWidget };
+});
