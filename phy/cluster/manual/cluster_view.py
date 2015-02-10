8df4e9f4cde81c013c3fee003dada8b6c4c01424
Cedric GESTES <cedric.gestes@gmail.com>
2015-02-10 18:37:10 +0100

    clusterview: generate random data

--- phy/cluster/manual/cluster_view.py
+++ phy/cluster/manual/cluster_view.py
@@ -9,7 +9,7 @@
 import numpy as np
 from IPython.utils.traitlets import Unicode, List
 from IPython.html.widgets import DOMWidget
-
+import random
 
 #------------------------------------------------------------------------------
 # Cluster view
@@ -21,3 +21,33 @@ class ClusterView(DOMWidget):
     clusters = List(sync=True)
     colors = List(sync=True)
     value = List(sync=True)
+
+    def __init__(self, *args, **kwargs):
+        super(ClusterView, self).__init__(*args, **kwargs)
+        self.clusters = self.fake_populate()
+
+    def _gen_cluster_info(self, num):
+        for i in range(4):
+            nbins = 41
+            bins = [None] * (nbins + 1)
+
+            bins[0] = 0
+            bins[nbins] = 0
+
+            for i in range(1, nbins/2):
+                binval = random.randint(0,120)
+                bins[i] = binval
+                bins[nbins-i] = binval
+
+        return { 'id': num,
+                 'quality': random.randint(0, 1000),
+                 'nchannels': random.randint(10, 1000),
+                 'nspikes': random.randint(500, 1000000),
+                 'ccg': bins }
+
+    def fake_populate(self):
+        clusters = []
+
+        for i in range(4):
+            clusters.append(self._gen_cluster_info(i))
+        return clusters
