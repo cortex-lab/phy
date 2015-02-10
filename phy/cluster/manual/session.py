07a3969e2d642fdbd48aa1fff1bc71b585e318e1
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-02-10 12:27:46 +0100

    WIP: refactoring session

--- phy/cluster/manual/session.py
+++ phy/cluster/manual/session.py
@@ -6,232 +6,99 @@
 # Imports
 #------------------------------------------------------------------------------
 
+import re
 from collections import defaultdict
-from functools import wraps
+from functools import wraps, partial
 
-import numpy as np
+from ...utils._misc import _fun_arg_count
 
-from ._history import GlobalHistory
-from .clustering import Clustering
-from .cluster_view import ClusterView
-from .cluster_info import ClusterMetadata
-from .selector import Selector
-from ...io.base_model import BaseModel
-from ...plot.waveforms import WaveformView
-from ...notebook.utils import load_css
+# Template for a decorator that accepts both @decorator and @decorator().
+# def my_decorator(func=None, arg=None):
+#     if func is None:
+#         return partial(my_decorator, arg=arg)
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         # TODO
+#         return func(*args, **kwargs)
+#     return wrapper
 
 
 #------------------------------------------------------------------------------
 # Session class
 #------------------------------------------------------------------------------
 
+def _get_on_name(func):
+    """Return 'eventname' when the function name is `on_<eventname>()`."""
+    r = re.match("^on_(.+)$", func.__name__)
+    if r:
+        event = r.group(1)
+    else:
+        raise ValueError("The function name should be "
+                         "`on_<eventname>`().")
+    return event
+
+
 class Session(object):
-    """Provide all user-exposed actions for a manual clustering session."""
-
-    def __init__(self, model):
-        if not isinstance(model, BaseModel):
-            raise ValueError("'model' must be an instance of a "
-                             "class deriving from BaseModel.")
-        # List of registered views.
-        self._views = []
-        # Dict (callback_type, view_class) => [list of callbacks]
+    """Provide actions, views, and an event system for creating an interactive
+    session."""
+    def __init__(self):
         self._callbacks = defaultdict(list)
-        # List of (create_callback, action_name) pairs.
-        self._view_creators = []
+        self._actions = []
 
-        self._global_history = GlobalHistory()
-        # Set the model and initialize the session.
-        self.model = model
-        self.update_after_load()
+    def connect(self, func=None):
+        """Decorator for a function reacting to an event being raised."""
+        if func is None:
+            return self.connect
 
-    @property
-    def views(self):
-        return self._views
+        # Get the event name from the function.
+        event = _get_on_name(func)
 
-    def unregister_view(self, view):
-        """Unregister a view instance."""
-        self._views.remove(view)
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
 
-    def callback(self, view_class=None):
-        """Return a decorator adding a callback function."""
-        def create_decorator(f):
-            # on_mytype
-            callback_type = f.__name__[3:]
-            self._callbacks[callback_type, view_class].append(f)
-            return f
-        return create_decorator
+        # We register the callback function.
+        self._callbacks[event].append(wrapper)
 
-    def create(self, action_name=None):
-        """Callback function creating a new view."""
-        def create_decorator(f):
+        return wrapper
 
-            # Check that the decorated function name is valid.
-            if hasattr(self, f.__name__):
-                raise ValueError("This function name already exists in "
-                                 "the Session: {0}.".format(f.__name__))
+    def action(self, func=None, name=None, event=None):
+        """Decorator for a callback function of an action.
 
-            # Wrapped function.
-            @wraps(f)
-            def _register_view():
-                # Create the view.
-                view = f()
-                # Register the view.
-                self._views.append(view)
-                # Call all 'load' and 'select' callbacks on that view.
-                # This is to make sure the view is automatically updated
-                # when it is created after the data has been loaded and
-                # some clusters have been selected.
-                # TODO: concatenate with itertools
-                for callback in self._iter_callbacks('load', view.__class__):
-                    self._call_callback_on_view(callback, view)
-                for callback in self._iter_callbacks('select', view.__class__):
-                    self._call_callback_on_view(callback, view)
+        It automatically raises an event named 'event'. If None, the name of
+        the event is the name of the function.
 
-                return view
+        The 'name' argument is used as a title for the GUI button.
 
-            # Assign the decorated view creator to the session.
-            setattr(self, f.__name__, _register_view)
+        """
+        if func is None:
+            return partial(self.action, name=name, event=event)
 
-            # Register the view creators with their names.
-            self._view_creators.append((_register_view, action_name))
+        # By default, the event name is the function name.
+        if event is None:
+            event = func.__name__
 
-            return _register_view
-        return create_decorator
+        # Register the action.
+        self._actions.append({'func': func, 'name': name, 'event': event})
 
-    def _iter_callbacks(self, callback_type, view_class=None):
-        # Callbacks registered to a particular view class.
-        if view_class is not None:
-            for item in self._callbacks[callback_type, view_class]:
-                yield item
-        # Callbacks registered to any view class.
-        for item in self._callbacks[callback_type, None]:
-            yield item
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # Execute the action.
+            out = func(*args, **kwargs)
+            # Raise an event with the output function as an argument.
+            self.emit(event, out)
+            return out
 
-    def _iter_views(self, view_class=None):
-        """Iterate over all views of a certain type."""
-        for view in self._views:
-            for view in self._views:
-                if (view_class is not None and
-                   not isinstance(view, view_class)):
-                    continue
-                yield view
+        # Set the action function as a Session method.
+        setattr(self, func.__name__, wrapper)
 
-    def _call_callback_on_view(self, callback, view, **kwargs):
-        """Call a callback item on a view."""
-        # # Only call the callback if the view is of the correct type.
-        # if (callback_item['view'] is not None and
-        #    not isinstance(view, callback_item['view'])):
-        #     return
-        # Call the callback function on the view, with possibly an
-        # 'up' instance as argument.
-        if 'up' in kwargs:
-            callback(view, up=kwargs['up'])
-        else:
-            callback(view)
+        return wrapper
 
-    def _call_callbacks(self, callback_type, **kwargs):
-        """Call all callbacks of a given type."""
-        # kwargs are arguments to pass to the callbacks.
-        # Loop over all views.
-        for view in self._iter_views():
-            # Loop over all callbacks of that type registered to that
-            # view class.
-            for callback in self._iter_callbacks(callback_type,
-                                                 view_class=view.__class__):
-                self._call_callback_on_view(callback, view, **kwargs)
-
-    # Controller.
-    # -------------------------------------------------------------------------
-
-    def update_after_load(self):
-        """Update the session after new data has been loaded."""
-        # TODO: call this after the channel groups has changed.
-        # Update the Selector and Clustering instances using the Model.
-        spike_clusters = self.model.spike_clusters
-        self.selector = Selector(spike_clusters, n_spikes_max=100)
-        self.clustering = Clustering(spike_clusters)
-        self.cluster_metadata = self.model.cluster_metadata
-        # Update all views.
-        self._call_callbacks('load')
-
-    def update_after_select(self):
-        """Update the views after the selection has changed."""
-        # Update all views.
-        self._call_callbacks('select')
-
-    def update_after_cluster(self, up, add_to_stack=True):
-        """Update the session after the clustering has changed."""
-
-        # TODO: Update the similarity matrix.
-        # stats.update(up)
-
-        # TODO: this doesn't do anything yet.
-        self.selector.update(up)
-
-        # TODO: this doesn't do anything yet.
-        # self.cluster_metadata.update(up)
-
-        if add_to_stack:
-            self._global_history.action(self.clustering)
-
-        # Update all views.
-        self._call_callbacks('cluster', up=up)
-
-    # Public properties.
-    # -------------------------------------------------------------------------
-
-    @property
-    def cluster_labels(self):
-        """Labels of all current clusters."""
-        return self.clustering.cluster_labels
-
-    @property
-    def cluster_colors(self):
-        """Colors of all current clusters."""
-        return [self.cluster_metadata[cluster]['color']
-                for cluster in self.clustering.cluster_labels]
-
-    # Public clustering actions.
-    # -------------------------------------------------------------------------
-
-    def select(self, clusters):
-        """Select some clusters."""
-        self.selector.selected_clusters = clusters
-        self.update_after_select()
-
-    def merge(self, clusters):
-        """Merge clusters."""
-        up = self.clustering.merge(clusters)
-        self.update_after_cluster(up)
-
-    def split(self, spikes):
-        """Create a new cluster from a selection of spikes."""
-        up = self.clustering.split(spikes)
-        self.update_after_cluster(up)
-
-    def move(self, clusters, group):
-        """Move clusters to a group."""
-        self.cluster_metadata.set(clusters, 'group', group)
-        self._global_history.action(self.cluster_metadata)
-
-    def undo(self):
-        """Undo the last action."""
-        up = self._global_history.undo()
-        self.update_after_cluster(up, add_to_stack=False)
-
-    def redo(self):
-        """Redo the last undone action."""
-        up = self._global_history.redo()
-        self.update_after_cluster(up, add_to_stack=False)
-
-    def wizard_start(self):
-        raise NotImplementedError()
-
-    def wizard_next(self):
-        raise NotImplementedError()
-
-    def wizard_previous(self):
-        raise NotImplementedError()
-
-    def wizard_reset(self):
-        raise NotImplementedError()
+    def emit(self, event, data=None):
+        """Call all callback functions registered for that event."""
+        for callback in self._callbacks[event]:
+            n_args = _fun_arg_count(callback)
+            if n_args == 0:
+                callback()
+            elif n_args == 1:
+                callback(data)
--- phy/cluster/manual/tests/test_session.py
+++ phy/cluster/manual/tests/test_session.py
@@ -9,10 +9,8 @@
 import os
 
 import numpy as np
-from numpy.testing import assert_array_equal as ae
 from pytest import raises
 
-from ....io.mock.artificial import artificial_spike_clusters, MockModel
 from ..session import Session
 
 
@@ -20,166 +18,39 @@ from ..session import Session
 # Tests
 #------------------------------------------------------------------------------
 
-class _BaseView(object):
-    is_loaded = False
-    is_selected = False
-    is_clustered = False
+def test_session_connect():
+    session = Session()
 
-    def show(self):
-        pass
-
-
-class _MyView(_BaseView):
-    pass
-
-
-class _MyViewBis(_BaseView):
-    pass
-
-
-def test_callback_manager():
-    model = MockModel()
-    session = Session(model)
-
-    # This function name is invalid.
+    # connect names should be on_something().
     with raises(ValueError):
-        @session.create('')
-        def clustering():
+        @session.connect
+        def invalid():
             pass
 
-    # Create views.
-    @session.create("Show me")
-    def show_me():
-        view = _MyView()
-        view.show()
-        return view
+    _track = []
 
-    @session.create("Show me bis")
-    def show_me_bis():
-        view = _MyViewBis()
-        view.show()
-        return view
+    @session.connect
+    def on_my_event():
+        _track.append("my event")
 
-    view = session.show_me()
-    session.show_me()
+    assert _track == []
 
-    assert len(session._views) == 2
+    session.emit("invalid")
+    assert _track == []
 
-    view_bis = session.show_me_bis()
-
-    # Test loading.
-    @session.callback(_MyView)
-    def on_load(view):
-        assert isinstance(view, _MyView)
-        view.is_loaded = True
-
-    assert not view.is_loaded
-    session.update_after_load()
-    assert view.is_loaded
-
-    # Test selection.
-    @session.callback()
-    def on_select(view):
-        assert isinstance(view, (_MyView, _MyViewBis))
-        view.is_selected = True
-
-    assert not view.is_selected
-    session.select([0])
-    assert view.is_selected
-
-    # Test cluster.
-    @session.callback(_MyViewBis)
-    def on_cluster(view, up=None):
-        assert isinstance(view, _MyViewBis)
-        view.is_clustered = True
-
-    assert not view_bis.is_clustered
-    session.merge([0])
-    assert view_bis.is_clustered
-
-    # Open a new window after the session has started.
-    new_view = session.show_me()
-    assert new_view.is_loaded
-    assert new_view.is_selected
-    assert not new_view.is_clustered
+    session.emit("my_event")
+    assert _track == ["my event"]
 
 
-def test_session():
+def test_session_connect_alternative():
+    session = Session()
 
-    # Mock model.
-    model = MockModel()
-    n_clusters = model.n_clusters
+    _track = []
 
-    with raises(ValueError):
-        Session(None)
+    assert _track == []
 
-    session = Session(model)
+    @session.connect()
+    def on_my_event():
+        _track.append("my event")
 
-    session.update_after_load()
-    ae(session.cluster_labels, np.arange(n_clusters))
-    assert len(session.cluster_colors) == n_clusters
-
-    # Selection.
-    session.select([1, 2])
-
-    clusters_0 = np.array(np.arange(n_clusters))
-    ae(session.clustering.cluster_labels, clusters_0)
-
-    # Action 1: merge.
-    session.merge([0, 1])
-    clusters_1 = np.arange(2, n_clusters + 1)
-    ae(session.clustering.cluster_labels, clusters_1)
-
-    # Action 2: merge.
-    session.merge([10, 2])
-    clusters_2 = np.array([3, 4, 5, 6, 7, 8, 9, 11])
-    ae(session.clustering.cluster_labels, clusters_2)
-
-    # Undo 2.
-    session.undo()
-    ae(session.clustering.cluster_labels, clusters_1)
-
-    # Undo 1.
-    session.undo()
-    ae(session.clustering.cluster_labels, clusters_0)
-
-    # Redo 1.
-    session.redo()
-    ae(session.clustering.cluster_labels, clusters_1)
-
-    # New set of actions.
-    # Split.
-    session.split([10, 20, 30])
-    clusters_2_bis = np.arange(2, 12)
-    ae(session.clustering.cluster_labels, clusters_2_bis)
-
-    # Undo
-    session.undo()
-    ae(session.clustering.cluster_labels, clusters_1)
-
-    # Redo
-    session.redo()
-    ae(session.clustering.cluster_labels, clusters_2_bis)
-
-    # Move two clusters.
-    session.move([9, 10], 1)
-    assert session.cluster_metadata.get(9, 'group') == 1
-    assert session.cluster_metadata.get(10, 'group') == 1
-
-    session.undo()
-    assert session.cluster_metadata.get(9, 'group') == 3
-    assert session.cluster_metadata.get(10, 'group') == 3
-
-    session.redo()
-    assert session.cluster_metadata.get(9, 'group') == 1
-    assert session.cluster_metadata.get(10, 'group') == 1
-
-    # Wizard actions not implemented yet.
-    with raises(NotImplementedError):
-        session.wizard_start()
-    with raises(NotImplementedError):
-        session.wizard_next()
-    with raises(NotImplementedError):
-        session.wizard_previous()
-    with raises(NotImplementedError):
-        session.wizard_reset()
+    assert _track == ["my event"]
