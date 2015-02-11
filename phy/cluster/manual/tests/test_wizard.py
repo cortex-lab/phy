4787a6dc6822585d31946230bbe9a28037eb942c
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-02-11 23:19:57 +0100

    WIP: wizard.

--- phy/cluster/manual/tests/test_wizard.py
+++ phy/cluster/manual/tests/test_wizard.py
@@ -7,6 +7,7 @@
 #------------------------------------------------------------------------------
 
 import numpy as np
+import numpy.random as npr
 from numpy.testing import assert_array_equal as ae
 from pytest import raises
 
@@ -18,4 +19,28 @@ from ..wizard import Wizard
 #------------------------------------------------------------------------------
 
 def test_wizard():
-    pass
+
+    n_channels = 8
+
+    def _masks(*ind_true):
+        m = np.zeros(n_channels, dtype=np.bool)
+        m[np.array(ind_true)] = True
+        return m
+
+    # 2-3: 1
+    # 2-5: 2
+    # 3-5: 3
+    cluster_stats = {2: {'quality': .9,
+                         'cluster_masks': _masks(1, 3)},
+                     3: {'quality': .3,
+                         'cluster_masks': _masks(2, 3, 4)},
+                     5: {'quality': .6,
+                         'cluster_masks': _masks(1, 2, 3, 4, 5)}}
+
+    wizard = Wizard(cluster_stats=cluster_stats,
+                    cluster_metadata={})
+
+    assert wizard.best_clusters() == [2, 5, 3]
+    assert wizard.best_cluster() == 2
+    assert wizard.most_similar_clusters(2) == [5, 3]
+    wizard.mark_dissimilar(2, 3)
--- phy/cluster/manual/wizard.py
+++ phy/cluster/manual/wizard.py
@@ -6,6 +6,9 @@
 # Imports
 #------------------------------------------------------------------------------
 
+import math
+from operator import itemgetter
+
 import numpy as np
 
 
@@ -13,18 +16,62 @@ import numpy as np
 # Wizard
 #------------------------------------------------------------------------------
 
-class Wizard(object):
-    def __init__(self, cluster_stats, cluster_metadata):
-        pass
+def _norm(x):
+    """Euclidean norm of a vector."""
+    return math.sqrt((x ** 2).sum())
 
-    def best_clusters(self, n_max=None):  # decreasing order of quality
-        pass
+
+def _cluster_masks_similarity(masks_ref, masks):
+    """Compute the similarity between two cluster masks."""
+    return np.dot(masks_ref / _norm(masks_ref),
+                  masks / _norm(masks))
+
+
+class Wizard(object):
+    def __init__(self, cluster_stats=None, cluster_metadata=None):
+        self._cluster_stats = cluster_stats
+        self._cluster_metadata = cluster_metadata
+
+    @property
+    def _clusters(self):
+        """Return the sorted list of valid clusters."""
+        clusters = self._cluster_stats.keys()
+        # Checking the keys in both dictionaries... probably not necessary.
+        # clusters_ = self._cluster_metadata.keys()
+        # assert clusters == clusters_
+        return clusters
+
+    def best_clusters(self, n_max=None):
+        """Return the list of best clusters sorted by decreasing quality."""
+        quality = [(cluster, self._cluster_stats[cluster]['quality'])
+                   for cluster in self._clusters]
+        quality_s = sorted(quality, key=itemgetter(1), reverse=True)
+        return [cluster for cluster, qual in quality_s[:n_max]]
 
     def best_cluster(self):
-        pass
+        """Return the best cluster."""
+        clusters = self.best_clusters(n_max=1)
+        if clusters:
+            return clusters[0]
 
     def most_similar_clusters(self, cluster, n_max=None):
-        pass
+        """Return the `n_max` most similar clusters."""
+        masks_ref = self._cluster_stats[cluster]['cluster_masks']
+        # TODO: select the appropriate groups
+        masks = [(other, self._cluster_stats[other]['cluster_masks'])
+                 for other in self._clusters
+                 if other != cluster]
+        similarity = [(clu, _cluster_masks_similarity(masks_ref, m))
+                      for (clu, m) in masks]
+        # TODO: refactor this snippet with best_clusters()
+        similarity_s = sorted(similarity, key=itemgetter(1), reverse=True)
+        return [clu for clu, sim in similarity_s[:n_max]]
 
-    def mark_dissimilar(self, clusters):
+    def mark_dissimilar(self, cluster_0, cluster_1):
+        """Mark two clusters as dissimilar after a human decision.
+
+        This pair should not be reproposed again to the user.
+
+        """
+        # TODO
         pass
