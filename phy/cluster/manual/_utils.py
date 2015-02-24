ee17344898c3c8f756fda7511d1f505f567b45f2
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-02-24 18:28:40 +0100

    Added _all_spikes_in_clusters() method.

--- phy/cluster/manual/_utils.py
+++ phy/cluster/manual/_utils.py
@@ -31,3 +31,22 @@ def _spikes_in_clusters(spike_clusters, clusters):
     if len(spike_clusters) == 0 or len(clusters) == 0:
         return np.array([], dtype=np.int)
     return np.nonzero(np.in1d(spike_clusters, clusters))[0]
+
+
+def _all_spikes_in_clusters(spike_clusters):
+    """Return a dictionary {cluster: list_of_spikes}."""
+    sorted_spikes = np.argsort(spike_clusters)
+    spike_clusters = spike_clusters[sorted_spikes]
+
+    diff = np.empty_like(spike_clusters)
+    diff[0] = 1
+    diff[1:] = np.diff(spike_clusters)
+
+    idx = np.nonzero(diff > 0)[0]
+    clusters = spike_clusters[idx]
+
+    spikes_in_clusters = {clusters[i]: np.sort(sorted_spikes[idx[i]:idx[i+1]])
+                          for i in range(len(clusters) - 1)}
+    spikes_in_clusters[clusters[-1]] = np.sort(sorted_spikes[idx[-1]:])
+
+    return spikes_in_clusters
