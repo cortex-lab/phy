cd646c98fd2cb60d3d59eadb7f4b42300afdce85
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-02-24 12:00:51 +0100

    Done DiskStore.

--- phy/cluster/manual/store.py
+++ phy/cluster/manual/store.py
@@ -6,9 +6,12 @@
 # Imports
 #------------------------------------------------------------------------------
 
+import os
 import os.path as op
+import shutil
 from collections import defaultdict
 
+from ...utils.logging import debug
 from ...io.h5 import open_h5
 from ...ext.six import string_types
 
@@ -35,7 +38,7 @@ class MemoryStore(object):
         else:
             if isinstance(keys, string_types):
                 return self._ds.get(cluster, {}).get(keys, None)
-            assert isinstance(keys, list)
+            assert isinstance(keys, (list, tuple))
             return {key: self._ds.get(cluster, {}).get(key, None)
                     for key in keys}
 
@@ -52,7 +55,7 @@ class MemoryStore(object):
 
 
 class DiskStore(object):
-    """Store cluster-related data on disk."""
+    """Store cluster-related data in HDF5 files."""
     def __init__(self, directory):
         self._directory = op.realpath(directory)
 
@@ -65,22 +68,81 @@ class DiskStore(object):
         rel_path = '{0:05d}.h5'.format(cluster)
         return op.realpath(op.join(self._directory, rel_path))
 
-    def _cluster_file(self, cluster):
+    def _cluster_file_exists(self, cluster):
+        """Return whether a cluster file exists."""
+        return op.exists(self._cluster_path(cluster))
+
+    def _cluster_file(self, cluster, mode):
         """Return a file handle of a cluster file."""
         path = self._cluster_path(cluster)
-        return open_h5(path, 'w')
+        # if mode == 'r' and not self._cluster_file_exists(cluster):
+        #     raise IOError("The cluster file does not exist.")
+        return open_h5(path, mode)
+
+    # Data get/set methods
+    # -------------------------------------------------------------------------
+
+    def _get(self, f, key):
+        """Return the data for a given key.
+
+        Can be overriden for custom on-disk format.
+
+        """
+        try:
+            return f.read('/{0:s}'.format(key))[...]
+        except IOError:
+            return None
+
+    def _set(self, f, key, value):
+        """Set the data for a given key.
+
+        Can be overriden for custom on-disk format.
+
+        """
+        # debug("Writing", key, str(value))
+        f.write('/{0:s}'.format(key), value, overwrite=True)
 
     # Public methods
     # -------------------------------------------------------------------------
 
     def store(self, cluster, **data):
         """Store cluster-related data."""
+        with self._cluster_file(cluster, 'a') as f:
+            for key, value in data.items():
+                self._set(f, key, value)
 
     def load(self, cluster, keys=None):
         """Load cluster-related data."""
+        # The cluster doesn't exist: return None for all keys.
+        if not self._cluster_file_exists(cluster):
+            if keys is None:
+                return {}
+            else:
+                return {key: None for key in keys}
+        # Create the output dictionary.
+        out = {}
+        # Open the cluster file in read mode.
+        with self._cluster_file(cluster, 'r') as f:
+            # If a single key is requested, return the value.
+            if isinstance(keys, string_types):
+                return self._get(f, keys)
+            # All keys are requested if None.
+            if keys is None:
+                keys = f.datasets()
+            assert isinstance(keys, (list, tuple))
+            # Fetch the values for all requested keys.
+            for key in keys:
+                out[key] = self._get(f, key)
+        return out
 
     def keys(self):
         """List of cluster ids in the store."""
+        files = os.listdir(self._directory)
+        clusters = [int(op.splitext(file)[0]) for file in files]
+        return sorted(clusters)
 
     def delete(self, clusters):
         """Delete some clusters from the store."""
+        for cluster in clusters:
+            if self._cluster_file_exists(cluster):
+                os.remove(self._cluster_path(cluster))
--- phy/cluster/manual/tests/test_store.py
+++ phy/cluster/manual/tests/test_store.py
@@ -6,6 +6,10 @@
 # Imports
 #------------------------------------------------------------------------------
 
+import numpy as np
+from numpy.testing import assert_array_equal as ae
+
+from ....utils.logging import set_level
 from ....utils.tempdir import TemporaryDirectory
 from ..store import MemoryStore, DiskStore
 
@@ -14,7 +18,8 @@ from ..store import MemoryStore, DiskStore
 # Test data stores
 #------------------------------------------------------------------------------
 
-def _test_store(ms):
+def test_memory_store():
+    ms = MemoryStore()
     assert ms.load(2) == {}
 
     assert ms.load(3).get('key', None) is None
@@ -43,12 +48,43 @@ def _test_store(ms):
     assert ms.keys() == []
 
 
-def test_memory_store():
-    ms = MemoryStore()
-    _test_store(ms)
-
-
 def test_disk_store():
+
+    a = np.random.rand(2, 4)
+    b = np.random.rand(3, 5)
+
+    def _assert_equal(d_0, d_1):
+        """Test the equality of two dictionaries containing NumPy arrays."""
+        assert sorted(d_0.keys()) == sorted(d_1.keys())
+        for key in d_0.keys():
+            ae(d_0[key], d_1[key])
+
     with TemporaryDirectory() as tempdir:
         ds = DiskStore(tempdir)
-        assert ds  # _test_store(ds)
+
+        assert ds.load(2) == {}
+
+        assert ds.load(3).get('key', None) is None
+        assert ds.load(3) == {}
+        assert ds.load(3, ['key']) == {'key': None}
+        assert ds.load(3) == {}
+        assert ds.keys() == []
+
+        ds.store(3, key=a)
+        _assert_equal(ds.load(3), {'key': a})
+        _assert_equal(ds.load(3, ['key']), {'key': a})
+        ae(ds.load(3, 'key'), a)
+        assert ds.keys() == [3]
+
+        ds.store(3, key_bis=b)
+        _assert_equal(ds.load(3), {'key': a, 'key_bis': b})
+        _assert_equal(ds.load(3, ['key']), {'key': a})
+        _assert_equal(ds.load(3, ['key_bis']), {'key_bis': b})
+        _assert_equal(ds.load(3, ['key', 'key_bis']), {'key': a, 'key_bis': b})
+        ae(ds.load(3, 'key_bis'), b)
+        assert ds.keys() == [3]
+
+        ds.delete([2, 3])
+        assert ds.load(3) == {}
+        assert ds.load(3, ['key']) == {'key': None}
+        assert ds.keys() == []
