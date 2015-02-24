5a96ccbf52323fc4b162af8e01c62e8ec21ea38d
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-02-24 10:55:05 +0100

    WIP: memory store.

--- phy/cluster/manual/store.py
+++ phy/cluster/manual/store.py
@@ -6,6 +6,37 @@
 # Imports
 #------------------------------------------------------------------------------
 
+from collections import defaultdict
+
+
 #------------------------------------------------------------------------------
 # Data stores
 #------------------------------------------------------------------------------
+
+class MemoryStore(object):
+    """Store cluster-related data in memory."""
+    def __init__(self):
+        self._ds = {}
+
+    def store(self, cluster, **data):
+        """Store cluster-related data."""
+        self._ds[cluster].update(data)
+
+    def load(self, cluster, keys=None):
+        """Load cluster-related data."""
+        if keys is None:
+            return self._ds.get(cluster, {})
+        else:
+            return {key: self._ds[cluster].get(key, None) for key in keys}
+
+    def delete(self, clusters):
+        """Delete some clusters from the store."""
+        for cluster in clusters:
+            if cluster in self._ds:
+                del self._ds[cluster]
+
+
+class DiskStore(object):
+    """Store cluster-related data on disk."""
+    def __init__(self, directory):
+        self._directory = directory
