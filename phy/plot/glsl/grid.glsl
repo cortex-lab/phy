c2d78328ec5887ede974b5b4f724149bbf2dc389
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-04-03 21:33:56 +0200

    Refactored grid.glsl.

--- phy/plot/glsl/grid.glsl
+++ phy/plot/glsl/grid.glsl
@@ -3,12 +3,17 @@ uniform float n_rows;
 
 varying vec2 v_position;
 
-vec2 to_box(vec2 position, float index) {
-    float col = mod(index, n_rows) + 0.5;
+vec2 row_col(float index, float n_rows) {
     float row = floor(index / n_rows) + 0.5;
+    float col = mod(index, n_rows) + 0.5;
+    return vec2(row, col);
+}
 
-    float x = -1.0 + col * (2.0 / n_rows);
-    float y = +1.0 - row * (2.0 / n_rows);
+vec2 to_box(vec2 position, float index) {
+    vec2 rc = row_col(index, n_rows);
+
+    float x = -1.0 + rc.y * (2.0 / n_rows);
+    float y = +1.0 - rc.x * (2.0 / n_rows);
 
     float width = 0.95 / (1.0 * n_rows);
     float height = 0.95 / (1.0 * n_rows);
