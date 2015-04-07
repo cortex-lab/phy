2c115aa7b462ce6c1bf07781b985d29c8c9e43d5
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-04-07 11:58:25 +0200

    Added phy.debug() function to enable debug logging mode.

--- phy/__init__.py
+++ phy/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .utils import default_logger
+from .utils.logging import default_logger, set_level
 
 __author__ = 'Kwik Team'
 __email__ = 'cyrille.rossant at gmail.com'
@@ -7,3 +7,11 @@ __version__ = '0.1.0-alpha'
 
 # Set up the default logger.
 default_logger()
+
+
+def debug(enable=True):
+    """Enable debug logging mode."""
+    if enable:
+        set_level('debug')
+    else:
+        set_level('info')
