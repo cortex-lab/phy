3decfaecc4f356502d5d4a0bea6c284a68ff5913
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-04-08 21:22:46 +0200

    Added default settings for manual clustering.

---
+++ phy/cluster/manual/default_settings.py
@@ -0,0 +1,14 @@
+# Number of time samples in a bin.
+manual_clustering.ccg_binsize = 20
+
+# Number of bins (odd number).
+manual_clustering.ccg_winsize_bins = 2 * 25 + 1
+
+# Number of time excerpts for the CCG.
+manual_clustering.ccg_n_excerpts = 100
+
+# Number of spikes in each excerpt.
+manual_clustering.ccg_excerpt_size = 100
+
+# Maximum number of spikes to display in the views.
+manual_clustering.n_spikes_max = 100
