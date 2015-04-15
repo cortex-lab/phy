eeba5d394a730207b381cc97142444122ac7127a
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-04-15 15:04:04 +0200

    Wizard docstring.

--- phy/cluster/manual/wizard.py
+++ phy/cluster/manual/wizard.py
@@ -38,6 +38,7 @@ def _best_clusters(clusters, quality, n_max=None):
 #------------------------------------------------------------------------------
 
 class Wizard(object):
+    """Propose a selection of high-quality clusters and merge candidates."""
     def __init__(self, cluster_ids=None):
         self._similarity = None
         self._quality = None
@@ -46,10 +47,12 @@ class Wizard(object):
 
     @property
     def cluster_ids(self):
+        """Array of cluster ids in the current clustering."""
         return self._cluster_ids
 
     @cluster_ids.setter
     def cluster_ids(self, cluster_ids):
+        """Update the array of cluster ids."""
         if isinstance(cluster_ids, np.ndarray):
             cluster_ids = cluster_ids.tolist()
         self._cluster_ids = sorted(cluster_ids)
@@ -77,19 +80,25 @@ class Wizard(object):
     #--------------------------------------------------------------------------
 
     def best_clusters(self, n_max=10):
-        """Return the list of best clusters sorted by decreasing quality."""
+        """Return the list of best clusters sorted by decreasing quality.
+
+        The registered quality function is used for the cluster quality.
+
+        """
         self._check_cluster_ids()
         return self._filter(_best_clusters(self._cluster_ids, self._quality,
                                            n_max=n_max))
 
     def best_cluster(self):
-        """Return the best cluster."""
+        """Return the best cluster according to the registered cluster
+        quality function."""
         clusters = self.best_clusters(n_max=1)
         if clusters:
             return clusters[0]
 
     def most_similar_clusters(self, cluster=None, n_max=10):
-        """Return the `n_max` most similar clusters."""
+        """Return the `n_max` most similar clusters to a given cluster
+        (the current best cluster by default)."""
         if cluster is None:
             cluster = self.best_cluster()
         self._check_cluster_ids()
@@ -105,7 +114,12 @@ class Wizard(object):
         return [clu for (_, clu) in pairs]
 
     def ignore(self, cluster_or_pair):
-        """Mark a cluster or a pair of clusters as ignored."""
+        """Mark a cluster or a pair of clusters as ignored.
+
+        This cluster or pair of clusters will not reappear in the list of
+        best clusters or most similar clusters.
+
+        """
         if not isinstance(cluster_or_pair, (integer_types, tuple)):
             raise ValueError("This function accepts a cluster id "
                              "or a pair of ids as argument.")
