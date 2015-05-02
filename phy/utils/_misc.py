70f4b476b1bcfd52e71823eb5176c1a25e355d0f
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-05-02 13:10:01 +0200

    Added utility functions to check whether we are in IPython and in interactive mode.

--- phy/utils/_misc.py
+++ phy/utils/_misc.py
@@ -8,10 +8,12 @@
 #------------------------------------------------------------------------------
 
 import os
+import sys
 import os.path as op
 from inspect import getargspec
 
 from ..ext.six import string_types
+from ..ext.six.moves import builtins
 
 
 #------------------------------------------------------------------------------
@@ -65,6 +67,30 @@ def _fun_arg_count(f):
     if args and args[0] == 'self':
         args = args[1:]
     return len(args)
+
+
+def _is_in_ipython():
+    return '__IPYTHON__' in dir(builtins)
+
+
+def _is_interactive():
+    """.Determine if the user requested interactive mode."""
+    # The Python interpreter sets sys.flags correctly, so use them!
+    if sys.flags.interactive:
+        return True
+
+    # IPython does not set sys.flags when -i is specified, so first
+    # check it if it is already imported.
+    if '__IPYTHON__' not in dir(builtins):
+        return False
+
+    # Then we check the application singleton and determine based on
+    # a variable it sets.
+    try:
+        from IPython.config.application import Application as App
+        return App.initialized() and App.instance().interact
+    except (ImportError, AttributeError):
+        return False
 
 
 #------------------------------------------------------------------------------
