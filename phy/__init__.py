0f73fe835a8b112403d6a863b7210b9ee2ef0770
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-05-06 12:13:06 +0200

    Updated main __init__ module.

--- phy/__init__.py
+++ phy/__init__.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
+# flake8: noqa
+
+"""Electrophysiological data analysis package in Python.
+
+phy is an open source electrophysiological data analysis package in Python
+for neuronal recordings made with high-density multielectrode arrays
+containing tens, hundreds, or thousands of channels.
+
+"""
+
 from .utils.logging import default_logger, set_level
 
 __author__ = 'Kwik Team'
 __email__ = 'cyrille.rossant at gmail.com'
 __version__ = '0.1.0-alpha'
+
+__all__ = ['debug', 'set_level']
+
 
 # Set up the default logger.
 default_logger()
