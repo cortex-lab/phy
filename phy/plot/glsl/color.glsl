92d2c9441fb85ef4be1d30ba65cda65798c09c9f
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-05-11 16:45:45 +0200

    Fixed color HSV in shaders.

--- phy/plot/glsl/color.glsl
+++ phy/plot/glsl/color.glsl
@@ -6,7 +6,7 @@ vec3 get_color(float cluster, sampler2D texture, float n_clusters) {
 vec3 color_mask(vec3 color, float mask) {
     vec3 hsv = rgb_to_hsv(color);
     // Change the saturation and value as a function of the mask.
-    hsv.y = mask;
-    hsv.z = .5 * (1. + mask);
+    hsv.y *= mask;
+    hsv.z *= .5 * (1. + mask);
     return hsv_to_rgb(hsv);
 }
