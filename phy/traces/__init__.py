0c8baff5f6aebc73db5713bc8a9ec3fe6af478d5
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-06-16 14:35:36 +0200

    Updated traces.__init__.

---
+++ phy/traces/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+"""Spike detection, waveform extraction."""
+
+from .detect import Thresholder, FloodFillDetector
+from .filter import Filter
+from .loader import WaveformLoader
