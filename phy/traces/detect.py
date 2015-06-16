b7a5f69a8caf5b20e8916d4dd22676547df9db6b
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-06-16 12:13:20 +0200

    WIP: thresholder class.

--- phy/traces/detect.py
+++ phy/traces/detect.py
@@ -47,12 +47,19 @@ class Thresholder(object):
         elif self._mode == 'both':
             return np.abs(data)
 
-    def __call__(self, data, threshold='default'):
+    def __call__(self, data, threshold=None):
+        # Use the only threshold by default (if there is only one).
+        if threshold is None:
+            assert len(self._thresholds) == 1
+            threshold = list(self._thresholds.keys())[0]
+        # Fetch the threshold from its name.
         if isinstance(threshold, string_types):
             assert threshold in self._thresholds
             threshold = self._thresholds[threshold]
         threshold = float(threshold)
+        # Transform the data according to the mode.
         data_t = self._transform(data)
+        # Threshold the data.
         return data_t > threshold
 
 
--- phy/traces/tests/test_detect.py
+++ phy/traces/tests/test_detect.py
@@ -23,6 +23,13 @@ def test_thresholder():
     strong, weak = .1, .2
 
     data = artificial_traces(n_samples, n_channels)
+
+    # Positive and strong.
     thresholder = Thresholder(mode='positive',
                               thresholds=strong)
     ae(thresholder(data), data > strong)
+
+    # Negative and weak.
+    thresholder = Thresholder(mode='negative',
+                              thresholds={'weak': weak})
+    ae(thresholder(data), data < -weak)
