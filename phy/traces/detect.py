8122533c587d9288a22a8d4c1513616efe724e68
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-06-16 12:46:09 +0200

    WIP: connected components.

--- phy/traces/detect.py
+++ phy/traces/detect.py
@@ -9,6 +9,7 @@
 import numpy as np
 
 from ..ext.six import string_types
+from ..ext.six.moves import range, zip
 
 
 #------------------------------------------------------------------------------
@@ -67,5 +68,163 @@ class Thresholder(object):
         return data_t > threshold
 
 
+# -----------------------------------------------------------------------------
+# Connected components
+# -----------------------------------------------------------------------------
+
+def _to_tuples(x):
+    return ((i, j) for (i, j) in x)
+
+
+def _to_list(x):
+    return [(i, j) for (i, j) in x]
+
+
+def connected_components(weak_crossings=None,
+                         strong_crossings=None,
+                         probe_adjacency_list=None,
+                         join_size=None):
+    """Return a list of pairs `(samp, chan)` of the connected components in
+    the 2D array `weak_crossings`, where a pair is adjacent if the samples are
+    within `join_size` of each other, and the channels are adjacent in
+    `probe_adjacency_list`, the channel graph.
+
+    Parameters
+    ----------
+
+    weak_crossings : array
+        `(n_samples, n_channels)` array with weak threshold crossings
+    strong_crossings : array
+        `(n_samples, n_channels)` array with strong threshold crossings
+    probe_adjacency_list : dict
+        A dict `{channel: [neighbors]}`
+    join_size : int
+        The number of samples defining the tolerance in time for
+        finding connected components
+
+    """
+
+    if probe_adjacency_list is None:
+        probe_adjacency_list = {}
+
+    if strong_crossings is None:
+        strong_crossings = weak_crossings
+
+    assert weak_crossings.shape == strong_crossings.shape
+
+    # Set of connected component labels which contain at least one strong
+    # node.
+    strong_nodes = set()
+
+    n_s, n_ch = weak_crossings.shape
+    join_size = int(join_size or 0)
+
+    # An array with the component label for each node in the array
+    label_buffer = np.zeros((n_s, n_ch), dtype=np.int32)
+
+    # Component indices, a dictionary with keys the label of the component
+    # and values a list of pairs (sample, channel) belonging to that component
+    comp_inds = {}
+
+    # mgraph is the channel graph, but with edge node connected to itself
+    # because we want to include ourself in the adjacency. Each key of the
+    # channel graph (a dictionary) is a node, and the value is a set of nodes
+    # which are connected to it by an edge
+    mgraph = {}
+    for source, targets in probe_adjacency_list.items():
+        # we add self connections
+        mgraph[source] = targets.union([source])
+
+    # Label of the next component
+    c_label = 1
+
+    # For all pairs sample, channel which are nonzero (note that numpy .nonzero
+    # returns (all_i_s, all_i_ch), a pair of lists whose values at the
+    # corresponding place are the sample, channel pair which is nonzero. The
+    # lists are also returned in sorted order, so that i_s is always increasing
+    # and i_ch is always increasing for a given value of i_s. izip is an
+    # iterator version of the Python zip function, i.e. does the same as zip
+    # but quicker. zip(A,B) is a list of all pairs (a,b) with a in A and b in B
+    # in order (i.e. (A[0], B[0]), (A[1], B[1]), .... In conclusion, the next
+    # line loops through all the samples i_s, and for each sample it loops
+    # through all the channels.
+    for i_s, i_ch in zip(*weak_crossings.nonzero()):
+        # The next two lines iterate through all the neighbours of i_s, i_ch
+        # in the graph defined by graph in the case of edges, and
+        # j_s from i_s-join_size to i_s.
+        for j_s in range(i_s - join_size, i_s + 1):
+            # Allow us to leave out a channel from the graph to exclude bad
+            # channels
+            if i_ch not in mgraph:
+                continue
+            for j_ch in mgraph[i_ch]:
+                # Label of the adjacent element.
+                adjlabel = label_buffer[j_s, j_ch]
+                # If the adjacent element is nonzero we need to do something.
+                if adjlabel:
+                    curlabel = label_buffer[i_s, i_ch]
+                    if curlabel == 0:
+                        # If current element is still zero, we just assign
+                        # the label of the adjacent element to the current one.
+                        label_buffer[i_s, i_ch] = adjlabel
+                        # And add it to the list for the labelled component.
+                        comp_inds[adjlabel].append((i_s, i_ch))
+
+                    elif curlabel != adjlabel:
+                        # If the current element is unequal to the adjacent
+                        # one, we merge them by reassigning the elements of the
+                        # adjacent component to the current one.
+                        # samps_chans is an array of pairs sample, channel
+                        # currently assigned to component adjlabel.
+                        samps_chans = np.array(comp_inds[adjlabel],
+                                               dtype=np.int32)
+
+                        # samps_chans[:, 0] is the sample indices, so this
+                        # gives only the samp,chan pairs that are within
+                        # join_size of the current point.
+                        # TODO: is this the right behaviour? If a component can
+                        # have a width bigger than join_size I think it isn't!
+                        samps_chans = samps_chans[i_s - samps_chans[:, 0]
+                                                  <= join_size]
+
+                        # Relabel the adjacent samp,chan points with current
+                        # label.
+                        samps, chans = samps_chans[:, 0], samps_chans[:, 1]
+                        label_buffer[samps, chans] = curlabel
+
+                        # Add them to the current label list, and remove the
+                        # adjacent component entirely.
+                        comp_inds[curlabel].extend(comp_inds.pop(adjlabel))
+
+                        # Did not deal with merge condition, now fixed it
+                        # seems...
+                        # WARNING: might this "in" incur a performance hit
+                        # here...?
+                        if adjlabel in strong_nodes:
+                            strong_nodes.add(curlabel)
+                            strong_nodes.remove(adjlabel)
+
+                    # NEW: add the current component label to the set of all
+                    # strong nodes, if the current node is strong.
+                    if curlabel > 0 and strong_crossings[i_s, i_ch]:
+                        strong_nodes.add(curlabel)
+
+        if label_buffer[i_s, i_ch] == 0:
+            # If nothing is adjacent, we have the beginnings of a new
+            # component, # so we label it, create a new list for the new
+            # component which is given label c_label,
+            # then increase c_label for the next new component afterwards.
+            label_buffer[i_s, i_ch] = c_label
+            comp_inds[c_label] = [(i_s, i_ch)]
+            if strong_crossings[i_s, i_ch]:
+                strong_nodes.add(c_label)
+            c_label += 1
+
+    # Only return the values, because we don't actually need the labels.
+    comps = [comp_inds[key] for key in comp_inds.keys() if key in strong_nodes]
+    return comps
+
+
 class FloodFillDetector(object):
-    pass
+    def __init__(self):
+        pass
--- phy/traces/tests/test_detect.py
+++ phy/traces/tests/test_detect.py
@@ -10,12 +10,12 @@
 import numpy as np
 from numpy.testing import assert_array_equal as ae
 
-from ..detect import Thresholder
+from ..detect import Thresholder, connected_components
 from ...io.mock import artificial_traces
 
 
 #------------------------------------------------------------------------------
-# Tests
+# Test thresholder
 #------------------------------------------------------------------------------
 
 def test_thresholder():
@@ -46,3 +46,196 @@ def test_thresholder():
     t = thresholder(data, ('weak', 'strong'))
     ae(t['weak'], np.abs(data) > weak)
     ae(t['strong'], np.abs(data) > strong)
+
+
+#------------------------------------------------------------------------------
+# Test connected components
+#------------------------------------------------------------------------------
+
+def _test_components(chunk=None, components=None, **kwargs):
+
+    def _clip(x, m, M):
+        return [_ for _ in x if m <= _ < M]
+
+    n = 5
+    probe_adjacency_list = {i: set(_clip([i - 1, i + 1], 0, n))
+                            for i in range(n)}
+
+    if chunk is None:
+        chunk = [[0, 0, 0, 0, 0],
+                 [0, 0, 1, 0, 0],
+                 [1, 0, 1, 1, 0],
+                 [1, 0, 0, 1, 0],
+                 [0, 1, 0, 1, 1],
+                 ]
+
+    if not isinstance(chunk, np.ndarray):
+        chunk = np.array(chunk)
+    strong_crossings = kwargs.pop('strong_crossings', None)
+    if strong_crossings is not None and not isinstance(strong_crossings, np.ndarray):
+        strong_crossings = np.array(strong_crossings)
+
+    comp = connected_components(chunk,
+                                probe_adjacency_list=probe_adjacency_list,
+                                strong_crossings=strong_crossings,
+                                **kwargs)
+    assert len(comp) == len(components), (len(comp), len(components))
+    for c1, c2 in zip(comp, components):
+        assert set(c1) == set(c2)
+
+
+def test_components():
+    # 1 time step, 1 element
+    _test_components([[0, 0, 0, 0, 0]],  [])
+
+    _test_components([[1, 0, 0, 0, 0]],  [[(0, 0)]])
+
+    _test_components([
+            [0, 1, 0, 0, 0],
+        ],  [[(0, 1)]])
+
+    _test_components([
+            [0, 0, 0, 1, 0],
+        ],  [[(0, 3)]])
+
+    _test_components([
+            [0, 0, 0, 0, 1],
+        ],  [[(0, 4)]])
+
+    # 1 time step, 2 elements
+    _test_components([
+            [1, 1, 0, 0, 0],
+        ],  [[(0, 0), (0, 1)]])
+
+    _test_components([
+            [1, 0, 1, 0, 0],
+        ],  [[(0, 0)], [(0, 2)]])
+
+    _test_components([
+            [1, 0, 0, 0, 1],
+        ],  [[(0, 0)], [(0, 4)]])
+
+    _test_components([
+            [0, 1, 0, 1, 0],
+        ],  [[(0, 1)], [(0, 3)]])
+
+    # 1 time step, 3 elements
+    _test_components([
+            [1, 1, 1, 0, 0],
+        ],  [[(0, 0), (0, 1), (0, 2)]])
+
+    _test_components([
+            [1, 1, 0, 1, 0],
+        ],  [[(0, 0), (0, 1)], [(0, 3)]])
+
+    _test_components([
+            [1, 0, 1, 1, 0],
+        ],  [[(0, 0)], [(0, 2), (0, 3)]])
+
+    _test_components([
+            [0, 1, 1, 1, 0],
+        ],  [[(0, 1), (0, 2), (0, 3)]])
+
+    _test_components([
+            [0, 1, 1, 0, 1],
+        ],  [[(0, 1), (0, 2)], [(0, 4)]])
+
+
+    # 5 time steps, varying join_size
+    _test_components([
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [1, 0, 1, 1, 0],
+            [1, 0, 0, 1, 0],
+            [0, 1, 0, 1, 1],
+        ],  [[(1, 2)],
+             [(2, 0)], [(2, 2), (2, 3)],
+             [(3, 0)], [(3, 3)],
+             [(4, 1)], [(4, 3), (4, 4)],
+             ])
+
+    _test_components([
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [1, 0, 1, 1, 0],
+            [1, 0, 0, 1, 0],
+            [0, 1, 0, 1, 1],
+        ],  [[(1, 2), (2, 2), (2, 3), (3, 3), (4, 3), (4, 4)],
+             [(2, 0), (3, 0), (4, 1)],
+             ],
+        join_size=1
+        )
+
+    _test_components(None,
+            [[(1, 2), (2, 2), (2, 3), (3, 3), (4, 3), (4, 4),
+              (2, 0), (3, 0), (4, 1)],
+             ],
+        join_size=2
+        )
+
+    # 5 time steps, strong != weak
+    _test_components(None,
+            [],
+        join_size=0,
+        strong_crossings=[
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        )
+
+    _test_components(None,
+            [[(1, 2)],
+             ],
+        join_size=0,
+        strong_crossings=[
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        )
+
+    _test_components(None,
+            [[(1, 2), (2, 2), (2, 3), (3, 3), (4, 3), (4, 4)],
+             ],
+        join_size=1,
+        strong_crossings=[
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1],
+        ]
+        )
+
+    _test_components(None,
+            [[(1, 2), (2, 2), (2, 3), (3, 3), (4, 3), (4, 4),
+              (2, 0), (3, 0), (4, 1)],
+             ],
+        join_size=2,
+        strong_crossings=[
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        )
+
+    _test_components(None,
+            [[(1, 2), (2, 2), (2, 3), (3, 3), (4, 3), (4, 4),
+              (2, 0), (3, 0), (4, 1)],
+             ],
+        join_size=2,
+        strong_crossings=[
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+        ]
+        )
