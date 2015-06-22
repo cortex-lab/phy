298f0c9304979603cf1820372ea23d3ef1c69e2c
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-06-22 14:25:21 +0200

    Test Kwik creator add spikes.

--- phy/io/kwik/creator.py
+++ phy/io/kwik/creator.py
@@ -19,7 +19,11 @@ from ...ext.six import string_types
 # Kwik creator
 #------------------------------------------------------------------------------
 
-def _write_by_chunk(dset, arrs):
+def _write_by_chunk(dset, arrs, transform=None):
+    # One can specify a transform function to apply to every chunk before
+    # it is copied to the HDF5 dataset.
+    if transform is None:
+        transform = lambda _: _
     assert dset
     assert isinstance(arrs, list)
     if len(arrs) == 0:
@@ -40,7 +44,7 @@ def _write_by_chunk(dset, arrs):
     offset = 0
     for arr in arrs:
         n = arr.shape[0]
-        dset[offset:offset + n, ...] = arr[...]
+        dset[offset:offset + n, ...] = transform(arr[...])
         offset += arr.shape[0]
     assert offset == dset.shape[0]
 
@@ -107,26 +111,35 @@ class KwikCreator(object):
         # Add features and masks in the .kwx file.
         assert masks is not None
         assert features is not None
+
         # Find n_channels and n_features.
         if isinstance(features, np.ndarray):
             _, n_channels, n_features = features.shape
         elif isinstance(features, list):
             assert features
             _, n_channels, n_features = features[0].shape
+
         # Determine the shape of the features_masks array.
         shape = (n_spikes, n_channels * n_features, 2)
+
+        def transform_f(f):
+            return f.reshape((-1, n_channels * n_features))
+
+        def transform_m(m):
+            return np.repeat(m, 3, axis=1)
+
         with open_h5(self.kwx_path, 'a') as f:
             fm = f.write('/channel_groups/{:d}/features_masks'.format(group),
                          shape=shape, dtype=np.float32)
 
             # Write the features either in one block, or chunk by chunk.
             if isinstance(features, np.ndarray):
-                fm[:, :, 0] = features
+                fm[:, :, 0] = transform_f(features)
             elif isinstance(features, list):
-                _write_by_chunk(fm[..., 0], features)
+                _write_by_chunk(fm[..., 0], features, transform_f)
 
             # Write the masks either in one block, or chunk by chunk.
             if isinstance(masks, np.ndarray):
-                fm[:, :, 1] = masks
+                fm[:, :, 1] = transform_m(masks)
             elif isinstance(masks, list):
-                _write_by_chunk(fm[..., 1], masks)
+                _write_by_chunk(fm[..., 1], masks, transform_m)
--- phy/io/kwik/tests/test_creator.py
+++ phy/io/kwik/tests/test_creator.py
@@ -13,8 +13,12 @@ from numpy.testing import assert_array_equal as ae
 from numpy.testing import assert_allclose as ac
 
 from ....utils.tempdir import TemporaryDirectory
-from ..creator import KwikCreator, _write_by_chunk
 from ...h5 import open_h5
+from ..creator import KwikCreator, _write_by_chunk
+from ..mock import (artificial_spike_samples,
+                    artificial_features,
+                    artificial_masks,
+                    )
 
 
 #------------------------------------------------------------------------------
@@ -47,10 +51,12 @@ def test_creator():
 
         creator = KwikCreator(basename)
 
+        # Test create empty files.
         creator.create_empty()
         assert op.exists(basename + '.kwik')
         assert op.exists(basename + '.kwx')
 
+        # Test metadata.
         creator.set_metadata('/application_data/spikedetekt',
                              a=1, b=2., c=[0, 1])
 
@@ -58,3 +64,18 @@ def test_creator():
             assert f.read_attr('/application_data/spikedetekt', 'a') == 1
             assert f.read_attr('/application_data/spikedetekt', 'b') == 2.
             ae(f.read_attr('/application_data/spikedetekt', 'c'), [0, 1])
+
+        # Test add spikes.
+        n_spikes = 100
+        n_channels = 8
+        n_features = 3
+
+        spike_samples = artificial_spike_samples(n_spikes)
+        features = artificial_features(n_spikes, n_channels, n_features)
+        masks = artificial_masks(n_spikes, n_channels)
+
+        creator.add_spikes(group=0,
+                           spike_samples=spike_samples,
+                           features=features,
+                           masks=masks,
+                           )
