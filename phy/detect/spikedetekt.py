8a1d79876064982737d943c7a1a04b7034f6abfd
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-07-18 20:36:39 +0200

    Fixed module docstring.

--- phy/detect/spikedetekt.py
+++ phy/detect/spikedetekt.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Automatic clustering algorithms."""
+"""SpikeDetekt algorithm."""
 
 #------------------------------------------------------------------------------
 # Imports
