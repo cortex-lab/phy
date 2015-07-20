2d91f8acda2c24584e657fee492659605007877b
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-07-20 19:21:56 +0200

    WIP: improving datasets module.

--- phy/utils/datasets.py
+++ phy/utils/datasets.py
@@ -10,12 +10,6 @@ import hashlib
 import os
 import os.path as op
 
-try:
-    import requests
-    from requests import get
-except ImportError:
-    get = None
-
 from .logging import debug, info, warn
 from .settings import _phy_user_dir, _ensure_dir_exists
 
@@ -31,7 +25,29 @@ _BASE_URL = {
 }
 
 
-def md5(path, blocksize=2 ** 20):
+def _save_stream(r, path):
+    with open(path, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+
+def _download(url, stream=None):
+    from requests import get
+    r = get(url, stream=stream)
+    if r.status_code != 200:
+        warn("Error while downloading `{}`.".format(url))
+        r.raise_for_status()
+    return r
+
+
+def download_text_file(url):
+    """Download a text file."""
+    return _download(url).text
+
+
+def _md5(path, blocksize=2 ** 20):
     """Compute the checksum of a file."""
     m = hashlib.md5()
     with open(path, 'rb') as f:
@@ -43,71 +59,25 @@ def md5(path, blocksize=2 ** 20):
     return m.hexdigest()
 
 
-def download_file(url, output=None, checksum=None):
-    """Download a binary file from an URL."""
-    if output is None:
-        output = url.split('/')[-1]
-    if op.exists(output):
-        info("The file {} already exists: skipping.".format(output))
+def _check_md5(path, checksum):
+    if checksum is None:
         return
-    if not get:
-        raise ImportError("Please install the requests package.")
+    return _md5(path) == checksum
+
+
+def _check_md5_of_url(output_path, url):
     try:
-        r = get(url, stream=True)
-    except (requests.exceptions.ConnectionError,
-            requests.exceptions.HTTPError,
-            ):
-        raise RuntimeError("Unable to download `{}`.".format(url))
-    if r.status_code != 200:
-        warn("Error while downloading `{}`.".format(url))
-        r.raise_for_status()
-    info("Downloading `{}` to `{}`...".format(url, output))
-    with open(output, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-                f.flush()
-    if checksum is not None:
-        if md5(output) != checksum:
-            raise RuntimeError("The checksum of the downloaded file doesn't "
-                               "match the provided checksum.")
+        checksum = download_text_file(url + '.md5')
+    except Exception:
+        checksum = None
+    finally:
+        if checksum:
+            if not _check_md5(output_path, checksum):
+                raise RuntimeError("The checksum of the downloaded file "
+                                   "doesn't match the provided checksum.")
 
 
-def _download(url):
-    if not get:
-        raise ImportError("Please install the requests package.")
-    try:
-        return get(url).text
-    except requests.exceptions.ConnectionError:
-        raise RuntimeError("Unable to download `{}`.".format(url))
-
-
-def _download_test_data(name, phy_user_dir=None, force=False):
-    phy_user_dir = phy_user_dir or _phy_user_dir()
-    dir = op.join(phy_user_dir, 'test_data')
-    _ensure_dir_exists(dir)
-    path = op.join(dir, name)
-    if not force and op.exists(path):
-        return path
-    url = _BASE_URL['github'] + 'test/' + name
-    download_file(url, output=path)
-    return path
-
-
-def download_sample_data(name, output_dir=None, base='cortexlab'):
-    """Download a sample dataset.
-
-    Parameters
-    ----------
-
-    name : str
-        Name of the sample dataset to download.
-    output_dir : str
-        The directory where to save the file.
-    base : str
-        The id of the base URL. Can be `'cortexlab'` or `'github'`.
-
-    """
+def _validate_output_dir(output_dir):
     if output_dir is None:
         output_dir = '.'
     if not output_dir.endswith('/'):
@@ -115,31 +85,74 @@ def download_sample_data(name, output_dir=None, base='cortexlab'):
     output_dir = op.realpath(op.dirname(output_dir))
     if not op.exists(output_dir):
         os.mkdir(output_dir)
-    name, ext = op.splitext(name)
-    if not ext:
-        ext_list = ('.kwik', '.kwx', '.raw.kwd')
-    else:
-        ext_list = (ext,)
-    outputs = []
-    for ext in ext_list:
-        url = _BASE_URL[base] + name + ext
-        output = op.join(output_dir, name + ext)
-        url_checksum = _BASE_URL[base] + name + ext + '.md5'
-        # Try to download the md5 hash.
-        try:
-            checksum = _download(url_checksum).split(' ')[0]
-        except RuntimeError as e:
-            warn("The md5 file could not be found at `{}`.".format(
-                 url_checksum))
-            debug(e)
-            checksum = None
-        # Try to download the requested file.
-        try:
-            download_file(url, output=output, checksum=checksum)
-            outputs.append(output)
-        except RuntimeError as e:
-            warn("The data file could not be found at `{}`.".format(
-                 url))
-            debug(e)
-    if outputs:
-        return outputs
+
+
+def download_file(url, output_path=None):
+    """Download a binary file from an URL.
+
+    The checksum will be downloaded from `URL + .md5`. If this download
+    succeeds, the file's MD5 will be compared to the expected checksum.
+
+    Parameters
+    ----------
+
+    url : str
+        The file's URL.
+    output_path : str or None
+        The path where the file is to be saved.
+
+    Returns
+    -------
+
+    output_path : str
+        The path where the file was downloaded.
+
+    """
+    if output_path is None:
+        output_path = url.split('/')[-1]
+    if op.exists(output_path):
+        info("The file {} already exists: skipping.".format(output_path))
+        return
+    info("Downloading {0}...".format(url))
+    r = _download(url, stream=True)
+    _save_stream(r, output_path)
+    _check_md5_of_url(output_path, url)
+    return output_path
+
+
+def download_test_data(name, phy_user_dir=None, force=False):
+    """Download a test file."""
+    phy_user_dir = phy_user_dir or _phy_user_dir()
+    dir = op.join(phy_user_dir, 'test_data')
+    _ensure_dir_exists(dir)
+    path = op.join(dir, name)
+    if not force and op.exists(path):
+        return path
+    url = _BASE_URL['github'] + 'test/' + name
+    download_file(url, output_path=path)
+    return path
+
+
+def download_sample_data(filename, output_dir=None, base='cortexlab'):
+    """Download a sample dataset.
+
+    Parameters
+    ----------
+
+    filename : str
+        Name of the sample dataset to download.
+    output_dir : str
+        The directory where to save the file.
+    base : str
+        The id of the base URL. Can be `'cortexlab'` or `'github'`.
+
+    """
+    output_dir = _validate_output_dir(output_dir)
+    filename, ext = op.splitext(filename)
+    url = _BASE_URL[base] + filename
+    output_path = op.join(output_dir, filename)
+    try:
+        download_file(url, output_path=output_path)
+    except Exception as e:
+        warn("An error occurred while downloading `{}` to `{}`: {}".format(
+             url, output_path, str(e)))
