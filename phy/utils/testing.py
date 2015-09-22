131dec9c11724ecee2ccaf637f14c3ba46350d54
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-22 10:25:37 +0200

    WIP: improving VisPy testing

--- phy/utils/testing.py
+++ phy/utils/testing.py
@@ -156,47 +156,28 @@ def _profile(prof, statement, glob, loc):
 # Testing VisPy canvas
 #------------------------------------------------------------------------------
 
-def _frame(canvas):
-    canvas.update()
-    canvas.app.process_events()
-    time.sleep(1. / 60.)
-
-
-def show_test(canvas, n_frames=2):
+def show_test(canvas):
     """Show a VisPy canvas for a fraction of second."""
-    with canvas as c:
-        show_test_run(c, n_frames)
+    with canvas:
+        # Interactive mode for tests.
+        if '-i' in sys.argv:
+            while not canvas._closed:
+                canvas.update()
+                canvas.app.process_events()
+                time.sleep(1. / 60)
+        else:
+            canvas.update()
+            canvas.app.process_events()
 
 
-def show_test_start(canvas):
-    """This is the __enter__ of with canvas."""
-    canvas.show()
-    canvas._backend._vispy_warmup()
+# TODO
+# def test_1(guibot):
+#     c = Canvas()
+#     guibot.add(c)
+#     guibot.wait(c)
 
 
-def show_test_run(canvas, n_frames=2):
-    """Display frames of a canvas."""
-    if n_frames == 0:
-        while not canvas._closed:
-            _frame(canvas)
-    else:
-        for _ in range(n_frames):
-            _frame(canvas)
-            if canvas._closed:
-                return
-
-
-def show_test_stop(canvas):
-    """This is the __exit__ of with canvas."""
-    # ensure all GL calls are complete
-    if not canvas._closed:
-        canvas._backend._vispy_set_current()
-        canvas.context.finish()
-        canvas.close()
-    time.sleep(0.025)  # ensure window is really closed/destroyed
-
-
-def show_colored_canvas(color, n_frames=5):
+def show_colored_canvas(color):
     """Show a transient VisPy canvas with a uniform background color."""
     from vispy import app, gloo
     c = app.Canvas()
@@ -205,4 +186,4 @@ def show_colored_canvas(color, n_frames=5):
     def on_draw(e):
         gloo.clear(color)
 
-    show_test(c, n_frames=n_frames)
+    show_test(c)
