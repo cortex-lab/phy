158f1c6d48162477ab7de706c005453fa9258ac9
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-22 11:23:49 +0200

    Update phy.cluster.algorithms

--- phy/cluster/algorithms/tests/test_klustakwik.py
+++ phy/cluster/algorithms/tests/test_klustakwik.py
@@ -6,27 +6,8 @@
 # Imports
 #------------------------------------------------------------------------------
 
-from ....utils.logging import set_level
-from ....io.kwik import KwikModel
-from ....io.kwik.mock import create_mock_kwik
+from phy.io.mock import artificial_features, artificial_masks
 from ..klustakwik import cluster
-
-
-#------------------------------------------------------------------------------
-# Fixtures
-#------------------------------------------------------------------------------
-
-def setup():
-    set_level('info')
-
-
-def teardown():
-    set_level('info')
-
-
-sample_rate = 10000
-n_samples = 25000
-n_channels = 4
 
 
 #------------------------------------------------------------------------------
@@ -34,18 +15,14 @@ n_channels = 4
 #------------------------------------------------------------------------------
 
 def test_cluster(tempdir):
+    n_channels = 4
     n_spikes = 100
-    filename = create_mock_kwik(tempdir,
-                                n_clusters=1,
-                                n_spikes=n_spikes,
-                                n_channels=8,
-                                n_features_per_channel=3,
-                                n_samples_traces=5000)
-    model = KwikModel(filename)
+    features = artificial_features(n_spikes, n_channels * 3)
+    masks = artificial_masks(n_spikes, n_channels * 3)
 
-    spike_clusters = cluster(model, num_starting_clusters=10)
+    spike_clusters = cluster(features, masks, num_starting_clusters=10)
     assert len(spike_clusters) == n_spikes
 
-    spike_clusters = cluster(model, num_starting_clusters=10,
+    spike_clusters = cluster(features, masks, num_starting_clusters=10,
                              spike_ids=range(100))
     assert len(spike_clusters) == 100
