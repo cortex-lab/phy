5a5ba8bbd484f427260f699101e5e754e4a6c5d1
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-22 13:45:36 +0200

    Increase coverage in color module

--- phy/utils/tests/test_color.py
+++ phy/utils/tests/test_color.py
@@ -8,7 +8,9 @@
 
 from pytest import mark
 
-from .._color import _random_color, _is_bright, _random_bright_color
+from .._color import (_random_color, _is_bright, _random_bright_color,
+                      _selected_clusters_colors,
+                      )
 from ..testing import show_colored_canvas
 
 
@@ -24,4 +26,11 @@ def test_random_color():
     color = _random_color()
     show_colored_canvas(color)
 
-    assert _is_bright(_random_bright_color())
+    for _ in range(10):
+        assert _is_bright(_random_bright_color())
+
+
+def test_selected_clusters_colors():
+    assert _selected_clusters_colors().ndim == 2
+    assert len(_selected_clusters_colors(3)) == 3
+    assert len(_selected_clusters_colors(10)) == 10
