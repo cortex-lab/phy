1cdc33a36328a42e55df5a4ca27497e932f2f181
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-22 16:41:40 +0200

    WIP: increase coverage in testing

--- phy/utils/tests/test_testing.py
+++ phy/utils/tests/test_testing.py
@@ -8,9 +8,11 @@
 
 import time
 
+import numpy as np
 from vispy.app import Canvas
 
 from ..testing import (benchmark, captured_output, show_test,
+                       _assert_equal,
                        )
 
 
@@ -22,6 +24,11 @@ def test_captured_output():
     with captured_output() as (out, err):
         print('Hello world!')
     assert out.getvalue().strip() == 'Hello world!'
+
+
+def test_assert_equal():
+    d = {'a': {'b': np.random.rand(5), 3: 'c'}, 'b': 2.}
+    _assert_equal(d, d.copy())
 
 
 def test_benchmark():
