225eb2acd9857e56611d20f3bb2708188068b066
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-23 20:54:15 +0200

    Add dock tests

--- phy/gui/tests/test_dock.py
+++ phy/gui/tests/test_dock.py
@@ -10,8 +10,9 @@ from pytest import mark
 
 from vispy import app
 
+from ..qt import Qt
 from ..dock import DockWindow
-from ...utils._color import _random_color
+from phy.utils._color import _random_color
 
 
 # Skip these tests in "make test-quick".
@@ -66,12 +67,14 @@ def test_dock_status_message(qtbot):
 
 def test_dock_state(qtbot):
     _gs = []
-    gui = DockWindow()
+    gui = DockWindow(size=(100, 100))
     qtbot.addWidget(gui)
+
+    _press = []
 
     @gui.shortcut('press', 'ctrl+g')
     def press():
-        pass
+        _press.append(0)
 
     gui.add_view(_create_canvas(), 'view1')
     gui.add_view(_create_canvas(), 'view2')
@@ -82,6 +85,10 @@ def test_dock_state(qtbot):
         _gs.append(gui.save_geometry_state())
 
     gui.show()
+    qtbot.waitForWindowShown(gui)
+
+    qtbot.keyPress(gui, Qt.Key_G, Qt.ControlModifier)
+    assert _press == [0]
 
     assert len(gui.list_views('view')) == 3
     assert gui.view_count() == {
