a249ad9b7986b9cb30702fcdfcd08d3c3731bfd4
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-23 21:22:05 +0200

    Remove GUI base module

--- phy/gui/__init__.py
+++ phy/gui/__init__.py
@@ -5,9 +5,3 @@
 
 from .qt import start_qt_app, run_qt_app, enable_qt
 from .dock import DockWindow
-
-from .base import (BaseViewModel,
-                   HTMLViewModel,
-                   WidgetCreator,
-                   BaseGUI,
-                   )
