d047e93fd135d912bbfacd50cf05d63fbd783e01
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-28 15:29:50 +0200

    WIP: ChunkedArray

--- phy/utils/tests/test_array.py
+++ phy/utils/tests/test_array.py
@@ -25,6 +25,9 @@ from ..array import (_unique,
                      excerpts,
                      data_chunk,
                      get_excerpts,
+                     ChunkedArray,
+                     from_dask_array,
+                     to_dask_array,
                      _range_from_slice,
                      _pad,
                      _load_arrays,
@@ -283,6 +286,20 @@ def test_regular_subset():
     ae(regular_subset(spikes, 100, offset=2), spikes)
     ae(regular_subset(spikes, 3), [2, 7, 17])
     ae(regular_subset(spikes, 3, offset=1), [3, 11])
+
+
+#------------------------------------------------------------------------------
+# Test chunked array
+#------------------------------------------------------------------------------
+
+def test_chunked_array():
+    from dask.array import from_array
+    arr = np.arange(10)
+    chunks = ((2, 3, 5),)
+    da = from_array(arr, chunks)
+    ca = from_dask_array(da)
+    da_bis = to_dask_array(ca, chunks)
+    assert da.chunks == da_bis.chunks
 
 
 #------------------------------------------------------------------------------
