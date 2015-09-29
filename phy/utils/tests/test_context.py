43aa4b2ccbbab93a116305ff53bbc6d0c825173f
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-29 21:03:19 +0200

    WIP: ipyparallel fixtures

--- phy/utils/tests/test_context.py
+++ phy/utils/tests/test_context.py
@@ -6,15 +6,18 @@
 # Imports
 #------------------------------------------------------------------------------
 
+import os
+
 import numpy as np
 from numpy.testing import assert_array_equal as ae
-from pytest import yield_fixture
+from pytest import fixture, yield_fixture, mark
+from ipyparallel.tests.clienttest import ClusterTestCase, add_engines
 
 from ..context import Context, _iter_chunks_dask
 
 
 #------------------------------------------------------------------------------
-# Test context
+# Fixtures
 #------------------------------------------------------------------------------
 
 @yield_fixture
@@ -22,6 +25,10 @@ def context(tempdir):
     ctx = Context('{}/cache/'.format(tempdir))
     yield ctx
 
+
+#------------------------------------------------------------------------------
+# Test context
+#------------------------------------------------------------------------------
 
 def test_iter_chunks_dask():
     from dask.array import from_array
@@ -49,3 +56,31 @@ def test_context_dask(context):
     da = from_array(x, chunks=(3,))
     res = context.map_dask_array(square, da)
     ae(res.compute(), x ** 2)
+
+
+#------------------------------------------------------------------------------
+# ipyparallel tests
+#------------------------------------------------------------------------------
+
+@yield_fixture(scope='module')
+def ipy_client():
+
+    def iptest_stdstreams_fileno():
+        return os.open(os.devnull, os.O_WRONLY)
+
+    # OMG-THIS-IS-UGLY-HACK: monkey-patch this global object to avoid
+    # using the nose iptest extension (we're using pytest).
+    # See https://github.com/ipython/ipython/blob/master/IPython/testing/iptest.py#L317-L319  # noqa
+    from ipyparallel import Client
+    import ipyparallel.tests
+    ipyparallel.tests.nose.iptest_stdstreams_fileno = iptest_stdstreams_fileno
+
+    # Start two engines engine (one is launched by setup()).
+    ipyparallel.tests.setup()
+    ipyparallel.tests.add_engines(1)
+    yield Client(profile='iptest')
+    ipyparallel.tests.teardown()
+
+
+def test_client(ipy_client):
+    print(ipy_client.ids)
