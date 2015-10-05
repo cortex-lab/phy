970d1875a77e8b653dc6bc256cc3de4bf4dd2ddd
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-05 18:57:41 +0200

    WIP: tests of the manual clustering plugin

--- phy/cluster/manual/tests/test_gui_plugins.py
+++ phy/cluster/manual/tests/test_gui_plugins.py
@@ -6,6 +6,8 @@
 # Imports
 #------------------------------------------------------------------------------
 
+from numpy.testing import assert_array_equal as ae
+
 from phy.gui.tests.test_gui import gui  # noqa
 
 
@@ -15,7 +17,8 @@ from phy.gui.tests.test_gui import gui  # noqa
 
 def test_manual_clustering(qtbot, gui, spike_clusters,  # noqa
                            cluster_metadata):
-    gui.attach('ManualClustering',
-               spike_clusters=spike_clusters,
-               cluster_metadata=cluster_metadata,
-               )
+    mc = gui.attach('ManualClustering',
+                    spike_clusters=spike_clusters,
+                    cluster_metadata=cluster_metadata,
+                    )
+    ae(mc.cluster_ids, [2, 3, 5, 7])
