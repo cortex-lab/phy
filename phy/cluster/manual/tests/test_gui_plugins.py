50b02d704c144d6d5abaa2fa92cf4c0b700c78d6
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-05 18:50:54 +0200

    Flakify

--- phy/cluster/manual/tests/test_gui_plugins.py
+++ phy/cluster/manual/tests/test_gui_plugins.py
@@ -6,14 +6,15 @@
 # Imports
 #------------------------------------------------------------------------------
 
-from phy.gui.tests.test_gui import gui
+from phy.gui.tests.test_gui import gui  # noqa
 
 
 #------------------------------------------------------------------------------
 # Test GUI plugins
 #------------------------------------------------------------------------------
 
-def test_manual_clustering(qtbot, gui, spike_clusters, cluster_metadata):
+def test_manual_clustering(qtbot, gui, spike_clusters,  # noqa
+                           cluster_metadata):
     gui.attach('ManualClustering',
                spike_clusters=spike_clusters,
                cluster_metadata=cluster_metadata,
