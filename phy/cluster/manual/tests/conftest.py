ddf7926dedaaeae1d8e279189ba1574704cc2cb6
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-08 11:28:25 +0200

    WIP: refactor wizard tests

--- phy/cluster/manual/tests/conftest.py
+++ phy/cluster/manual/tests/conftest.py
@@ -8,8 +8,7 @@
 
 from pytest import yield_fixture
 
-from ..wizard import Wizard, _wizard_group, best_quality_strategy
-from .._utils import create_cluster_meta
+from ..wizard import Wizard
 
 
 #------------------------------------------------------------------------------
@@ -17,56 +16,38 @@ from .._utils import create_cluster_meta
 #------------------------------------------------------------------------------
 
 @yield_fixture
-def cluster_groups():
-    data = {1: 'noise',
-            2: 'mua',
-            11: 'good',
-            12: 'good',
-            13: 'good',
-            101: None,
-            102: None,
-            103: None,
-            104: None,
-            105: None,
-            }
-    yield data
+def cluster_ids():
+    yield [0, 1, 2, 10, 20, 30]
+    #      i, g, N, N,  N,  N
 
 
 @yield_fixture
-def cluster_meta(cluster_groups):
-    yield create_cluster_meta(cluster_groups)
+def get_cluster_ids(cluster_ids):
+    yield lambda: cluster_ids
 
 
 @yield_fixture
-def mock_wizard():
+def status():
+    yield lambda c: ('ignored', 'good')[c] if c <= 1 else None
 
+
+@yield_fixture
+def quality():
+    yield lambda c: c
+
+
+@yield_fixture
+def similarity():
+    yield lambda c, d: c * 1.01 + d
+
+
+@yield_fixture
+def wizard(get_cluster_ids, status, quality, similarity):
     wizard = Wizard()
-    wizard.set_cluster_ids_function(lambda: [1, 2, 3])
 
-    @wizard.set_quality_function
-    def quality(cluster):
-        return cluster
-
-    @wizard.set_similarity_function
-    def similarity(cluster, other):
-        return cluster + other
+    wizard.set_cluster_ids_function(get_cluster_ids)
+    wizard.set_status_function(status)
+    wizard.set_quality_function(quality)
+    wizard.set_similarity_function(similarity)
 
     yield wizard
-
-
-@yield_fixture
-def wizard_with_groups(mock_wizard, cluster_groups):
-
-    def get_cluster_ids():
-        return sorted(cluster_groups.keys())
-
-    mock_wizard.set_cluster_ids_function(get_cluster_ids)
-
-    @mock_wizard.set_status_function
-    def status(cluster):
-        group = cluster_groups.get(cluster, None)
-        return _wizard_group(group)
-
-    mock_wizard.set_strategy_function(best_quality_strategy)
-
-    yield mock_wizard
