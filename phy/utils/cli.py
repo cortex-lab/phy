15b12f2bbeb50b2c21c798c8c1aeb318ed261969
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-15 15:44:19 +0200

    WIP

--- phy/utils/cli.py
+++ phy/utils/cli.py
@@ -9,7 +9,9 @@
 #------------------------------------------------------------------------------
 
 import click
+
 import phy
+from phy.plugins import get_all_plugins
 
 
 #------------------------------------------------------------------------------
@@ -21,3 +23,20 @@ import phy
 @click.help_option()
 def phy():
     return 0
+
+
+#------------------------------------------------------------------------------
+# CLI plugins
+#------------------------------------------------------------------------------
+
+def load_cli_plugins(cli):
+    """Load all plugins and attach them to a CLI object."""
+    plugins = get_all_plugins()
+    # TODO: try/except to avoid crashing if a plugin is broken.
+    for plugin in plugins:
+        # NOTE: plugin is a class, so we need to instantiate it.
+        plugin().attach_to_cli(cli)
+
+
+# Load all plugins for the phy CLI.
+load_cli_plugins(phy)
