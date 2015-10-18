af12a90982890104a33241efc78d964e6bd009b9
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-18 19:13:43 +0200

    WIP: test plot utils

--- phy/plot/tests/test_utils.py
+++ phy/plot/tests/test_utils.py
@@ -7,7 +7,14 @@
 # Imports
 #------------------------------------------------------------------------------
 
-from ..utils import _load_shader
+import numpy as np
+from numpy.testing import assert_array_equal as ae
+from vispy import gloo
+
+from ..utils import (_load_shader,
+                     _tesselate_histogram,
+                     _enable_depth_mask,
+                     )
 
 
 #------------------------------------------------------------------------------
@@ -16,3 +23,23 @@ from ..utils import _load_shader
 
 def test_load_shader():
     assert 'main()' in _load_shader('ax.vert')
+
+
+def test_tesselate_histogram():
+    n = 5
+    hist = np.arange(n)
+    thist = _tesselate_histogram(hist)
+    assert thist.shape == (5 * n + 1, 2)
+    ae(thist[0], [-1, -1])
+    ae(thist[-1], [1, -1])
+
+
+def test_enable_depth_mask(qtbot, canvas):
+
+    @canvas.connect
+    def on_draw(e):
+        _enable_depth_mask()
+
+    canvas.show()
+    qtbot.waitForWindowShown(canvas.native)
+    # qtbot.stop()
