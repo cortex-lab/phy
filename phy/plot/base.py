04c51c2186630a393c1a9e0c858547cf643e60dd
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-19 18:23:00 +0200

    WIP: BaseCanvas

--- phy/plot/base.py
+++ phy/plot/base.py
@@ -9,6 +9,9 @@
 
 import logging
 
+from vispy import gloo
+from vispy.app import Canvas
+
 from .utils import _create_program
 
 logger = logging.getLogger(__name__)
@@ -18,6 +21,21 @@ logger = logging.getLogger(__name__)
 # Base spike visual
 #------------------------------------------------------------------------------
 
+class BaseCanvas(Canvas):
+    def __init__(self, *args, **kwargs):
+        super(BaseCanvas, self).__init__(*args, **kwargs)
+        self._visuals = []
+
+    def add_visual(self, visual):
+        self._visuals.append(visual)
+        visual.attach(self)
+
+    def on_draw(self, e):
+        gloo.clear()
+        for visual in self._visuals:
+            visual.draw()
+
+
 class BaseVisual(object):
     _gl_primitive_type = None
     _shader_name = None
@@ -26,37 +44,47 @@ class BaseVisual(object):
         assert self._gl_primitive_type
         assert self._shader_name
 
-        self._data = {'a_position': None}
-        self._to_upload = []  # list of arrays/params to upload
+        self.size = 1, 1
+        self._canvas = None
+        self._do_show = False
 
         self.program = _create_program(self._shader_name)
 
-    def is_empty(self):
-        """Return whether the visual is empty."""
-        return self._data['a_position'] is not None
+    def show(self):
+        self._do_show = True
+
+    def hide(self):
+        self._do_show = False
 
     def set_data(self):
+        """Set the data for the visual."""
         pass
 
     def set_transforms(self):
+        """Set the list of transforms for the visual."""
         pass
 
     def attach(self, canvas):
-        canvas.connect(self.on_draw)
+        """Attach some events."""
+        self._canvas = canvas
 
         @canvas.connect
         def on_resize(event):
             """Resize the OpenGL context."""
+            self.size = event.size
             canvas.context.set_viewport(0, 0, event.size[0], event.size[1])
 
-    def on_draw(self, e):
+        canvas.connect(self.on_mouse_move)
+
+    def on_mouse_move(self, e):
+        pass
+
+    def draw(self):
         """Draw the waveforms."""
-        # Upload to the GPU what needs to be uploaded.
-        for name in self._to_upload:
-            value = self._data[name]
-            logger.debug("Upload `%s`: %s.", name, str(value))
-            self.program[name] = value
-        # Reset the list of objects to upload.
-        self._to_upload = []
-        if not self.is_empty():
+        if not self._do_show:
             self.program.draw(self._gl_primitive_type)
+
+    def update(self):
+        """Trigger a draw event in the canvas from the visual."""
+        if self._canvas:
+            self._canvas.update()
