42b8f0935d0b31b9e9c798774110084175aabd00
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-19 19:16:20 +0200

    Increase coverage

--- phy/plot/tests/test_base.py
+++ phy/plot/tests/test_base.py
@@ -25,15 +25,14 @@ def test_base_visual(qtbot, canvas):
         def set_data(self):
             self.program['a_position'] = [[-1, 0, 0], [1, 0, 0]]
             self.program['n_rows'] = 1
-
-        def on_mouse_move(self, e):
-            y = 1 - 2 * e.pos[1] / float(self.size[1])
-            self.program['a_position'] = [[-1, y, 0], [1, y, 0]]
-            self.update()
+            self.show()
 
     v = TestVisual()
     v.set_data()
     canvas.add_visual(v)
 
     canvas.show()
-    # qtbot.stop()
+    v.hide()
+    v.show()
+    qtbot.waitForWindowShown(canvas.native)
+    v.update()
