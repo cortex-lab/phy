d732760b4021fca8110acbe21cb54a9e7aaa61d3
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-20 15:51:36 +0200

    Add TransformChain

--- phy/plot/tests/test_transform.py
+++ phy/plot/tests/test_transform.py
@@ -10,8 +10,12 @@
 from textwrap import dedent
 
 import numpy as np
+from numpy.testing import assert_equal as ae
+from pytest import yield_fixture
 
-from ..transform import Translate, Scale, Range, Clip, Subplot
+from ..transform import (Translate, Scale, Range, Clip, Subplot, GPU,
+                         TransformChain,
+                         )
 
 
 #------------------------------------------------------------------------------
@@ -47,15 +51,15 @@ def test_types():
             _check(t, arr, [[4, 6]], translate=[1, 2])
 
 
-def test_translate_numpy():
+def test_translate_cpu():
     _check(Translate(translate=[1, 2]), [3, 4], [[4, 6]])
 
 
-def test_scale_numpy():
+def test_scale_cpu():
     _check(Scale(), [3, 4], [[-3, 8]], scale=[-1, 2])
 
 
-def test_range_numpy():
+def test_range_cpu():
     kwargs = dict(from_range=[0, 0, 1, 1], to_range=[-1, -1, 1, 1])
 
     _check(Range(), [-1, -1], [[-3, -3]], **kwargs)
@@ -66,8 +70,10 @@ def test_range_numpy():
     _check(Range(), [[0, .5], [1.5, -.5]], [[-1, 0], [2, -2]], **kwargs)
 
 
-def test_clip_numpy():
+def test_clip_cpu():
     kwargs = dict(bounds=[0, 1, 2, 3])
+
+    _check(Clip(), [0, 0], [0, 0])  # Default bounds.
 
     _check(Clip(), [0, 1], [0, 1], **kwargs)
     _check(Clip(), [1, 2], [1, 2], **kwargs)
@@ -78,7 +84,7 @@ def test_clip_numpy():
     _check(Clip(), [[-1, 0], [3, 4]], [], **kwargs)
 
 
-def test_subplot_numpy():
+def test_subplot_cpu():
     shape = (2, 3)
 
     _check(Subplot(), [-1, -1], [-1, +0], index=(0, 0), shape=shape)
@@ -104,11 +110,13 @@ def test_scale_glsl():
 
 
 def test_range_glsl():
+
+    assert Range(from_range=[-1, -1, 1, 1]).glsl('x')
+
     expected = ('u_to.xy + (u_to.zw - u_to.xy) * (x - u_from.xy) / '
                 '(u_from.zw - u_from.xy)')
-    assert expected in Range().glsl('x',
-                                    from_range=['u_from.xy', 'u_from.zw'],
-                                    to_range=['u_to.xy', 'u_to.zw'])
+    r = Range(to_range=['u_to.xy', 'u_to.zw'])
+    assert expected in r.glsl('x', from_range=['u_from.xy', 'u_from.zw'])
 
 
 def test_clip_glsl():
@@ -126,3 +134,63 @@ def test_clip_glsl():
 def test_subplot_glsl():
     glsl = Subplot().glsl('x', shape='u_shape', index='a_index')
     assert 'x = ' in glsl
+
+
+#------------------------------------------------------------------------------
+# Test transform chain
+#------------------------------------------------------------------------------
+
+@yield_fixture
+def array():
+    yield np.array([[-1, 0], [1, 2]])
+
+
+def test_transform_chain_empty(array):
+    t = TransformChain([])
+
+    assert t.cpu_transforms == []
+    assert t.gpu_transforms == []
+    assert t.get('GPU') is None
+
+    ae(t.apply(array), array)
+    assert t.glsl('position') == ''
+
+
+def test_transform_chain_one(array):
+    translate = Translate(translate=[1, 2])
+    t = TransformChain([translate])
+
+    assert t.cpu_transforms == [translate]
+    assert t.gpu_transforms == []
+
+    ae(t.apply(array), [[0, 2], [2, 4]])
+
+
+def test_transform_chain_two(array):
+    translate = Translate(translate=[1, 2])
+    scale = Scale(scale=[.5, .5])
+    t = TransformChain([translate, scale])
+
+    assert t.cpu_transforms == [translate, scale]
+    assert t.gpu_transforms == []
+
+    assert isinstance(t.get('Translate'), Translate)
+    assert t.get('Unknown') is None
+
+    ae(t.apply(array), [[0, 1], [1, 2]])
+
+
+def test_transform_chain_complete(array):
+    t = TransformChain([Scale(scale=.5),
+                        Scale(scale=2.)])
+    t.add([Range(from_range=[-3, -3, 1, 1]),
+           GPU(),
+           Clip(),
+           Subplot(shape='u_shape', index='a_box'),
+           ])
+
+    assert len(t.cpu_transforms) == 3
+    assert len(t.gpu_transforms) == 2
+
+    ae(t.apply(array), [[0, .5], [1, 1.5]])
+    assert 'position = ' in t.glsl('position')
