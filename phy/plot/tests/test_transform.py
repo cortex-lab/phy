6609aab3f406459c71d6ea49542b397619951943
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-20 13:58:51 +0200

    WIP: refactoring transforms

--- phy/plot/tests/test_transform.py
+++ phy/plot/tests/test_transform.py
@@ -18,16 +18,19 @@ from ..transform import Translate, Scale, Range, Clip
 # Fixtures
 #------------------------------------------------------------------------------
 
-def _check(transform, array, expected):
-    transformed = transform.apply(array)
+def _check(transform, array, expected, **kwargs):
+    transformed = transform.apply(array, **kwargs)
     if array is None or not len(array):
         assert transformed == array
         return
     array = np.atleast_2d(array)
     if isinstance(array, np.ndarray):
-        assert transformed.shape == array.shape
+        assert transformed.shape[1] == array.shape[1]
         assert transformed.dtype == np.float32
-    assert np.all(transformed == expected)
+    if not len(transformed):
+        assert not len(expected)
+    else:
+        assert np.all(transformed == expected)
 
 
 #------------------------------------------------------------------------------
@@ -35,43 +38,44 @@ def _check(transform, array, expected):
 #------------------------------------------------------------------------------
 
 def test_types():
-    t = Translate([1, 2])
-    _check(t, [], [])
+    t = Translate()
+    _check(t, [], [], translate=[1, 2])
 
     for ab in [[3, 4], [3., 4.]]:
         for arr in [ab, [ab], np.array(ab), np.array([ab]),
                     np.array([ab, ab, ab])]:
-            _check(t, arr, [[4, 6]])
+            _check(t, arr, [[4, 6]], translate=[1, 2])
 
 
 def test_translate_numpy():
-    t = Translate([1, 2])
-    _check(t, [3, 4], [[4, 6]])
+    _check(Translate(), [3, 4], [[4, 6]], translate=[1, 2])
 
 
 def test_scale_numpy():
-    t = Scale([-1, 2])
-    _check(t, [3, 4], [[-3, 8]])
+    _check(Scale(), [3, 4], [[-3, 8]], scale=[-1, 2])
 
 
 def test_range_numpy():
-    t = Range([0, 0, 1, 1], [-1, -1, 1, 1])
+    kwargs = dict(from_range=[0, 0, 1, 1], to_range=[-1, -1, 1, 1])
 
-    _check(t, [-1, -1], [[-3, -3]])
-    _check(t, [0, 0], [[-1, -1]])
-    _check(t, [0.5, 0.5], [[0, 0]])
-    _check(t, [1, 1], [[1, 1]])
+    _check(Range(), [-1, -1], [[-3, -3]], **kwargs)
+    _check(Range(), [0, 0], [[-1, -1]], **kwargs)
+    _check(Range(), [0.5, 0.5], [[0, 0]], **kwargs)
+    _check(Range(), [1, 1], [[1, 1]], **kwargs)
 
-    _check(t, [[0, .5], [1.5, -.5]], [[-1, 0], [2, -2]])
+    _check(Range(), [[0, .5], [1.5, -.5]], [[-1, 0], [2, -2]], **kwargs)
 
 
 def test_clip_numpy():
-    t = Clip([0, 1, 2, 3])
+    kwargs = dict(bounds=[0, 1, 2, 3])
 
-    _check(t, [-1, -1], [[0, 1]])
-    _check(t, [3, 4], [[2, 3]])
+    _check(Clip(), [0, 1], [0, 1], **kwargs)
+    _check(Clip(), [1, 2], [1, 2], **kwargs)
+    _check(Clip(), [2, 3], [2, 3], **kwargs)
 
-    _check(t, [[-1, 0], [3, 4]], [[0, 1], [2, 3]])
+    _check(Clip(), [-1, -1], [], **kwargs)
+    _check(Clip(), [3, 4], [], **kwargs)
+    _check(Clip(), [[-1, 0], [3, 4]], [], **kwargs)
 
 
 #------------------------------------------------------------------------------
@@ -79,24 +83,24 @@ def test_clip_numpy():
 #------------------------------------------------------------------------------
 
 def test_translate_glsl():
-    t = Translate('u_translate')
-    assert t.glsl('x') == 'x + u_translate'
+    assert 'x = x + u_translate' in Translate().glsl('x',
+                                                     translate='u_translate')
 
 
 def test_scale_glsl():
-    t = Scale('u_scale')
-    assert t.glsl('x') == 'x * u_scale'
+    assert 'x = x * u_scale' in Scale().glsl('x', scale='u_scale')
 
 
 def test_range_glsl():
-    t = Range(['u_from.xy', 'u_from.zw'], ['u_to.xy', 'u_to.zw'])
-    assert t.glsl('x') == ('u_to.xy + (u_to.zw - u_to.xy) * (x - u_from.xy) / '
-                           '(u_from.zw - u_from.xy)')
+    expected = ('u_to.xy + (u_to.zw - u_to.xy) * (x - u_from.xy) / '
+                '(u_from.zw - u_from.xy)')
+    assert expected in Range().glsl('x',
+                                    from_range=['u_from.xy', 'u_from.zw'],
+                                    to_range=['u_to.xy', 'u_to.zw'])
 
 
 def test_clip_glsl():
-    t = Clip(['xymin', 'xymax'])
-    assert t.glsl('x') == dedent("""
+    expected = dedent("""
         if ((x.x < xymin.x) |
             (x.y < xymin.y) |
             (x.x > xymax.x) |
@@ -104,3 +108,4 @@ def test_clip_glsl():
             discard;
         }
         """).strip()
+    assert expected in Clip().glsl('x', bounds=['xymin', 'xymax'])
