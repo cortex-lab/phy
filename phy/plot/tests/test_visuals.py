a1fe33d87f8a82b5a0f48af505d12b01d82f3509
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-24 12:31:08 +0200

    Start scatter visual

---
+++ phy/plot/tests/test_visuals.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+"""Test visuals."""
+
+
+#------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------
+
+import numpy as np
+from pytest import mark
+
+from ..visuals import ScatterVisual
+
+
+#------------------------------------------------------------------------------
+# Fixtures
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# Test visuals
+#------------------------------------------------------------------------------
+
+@mark.parametrize('marker_type', ScatterVisual._supported_marker_types)
+def test_scatter_markers(qtbot, canvas_pz, marker_type):
+
+    # Try all marker types.
+    v = ScatterVisual(marker_type=marker_type)
+    v.attach(canvas_pz)
+
+    n = 100
+    pos = .2 * np.random.randn(n, 2)
+    v.set_data(pos=pos)
+
+    canvas_pz.show()
+    # qtbot.stop()
+
+
+def test_scatter_custom(qtbot, canvas_pz):
+
+    v = ScatterVisual()
+    v.attach(canvas_pz)
+
+    n = 100
+
+    # Random position.
+    pos = .2 * np.random.randn(n, 2)
+
+    # Random colors.
+    c = np.random.uniform(.4, .7, size=(n, 4))
+    c[:, -1] = .5
+
+    # Random sizes
+    s = 5 + 20 * np.random.rand(n)
+
+    v.set_data(pos=pos, colors=c, size=s)
+
+    canvas_pz.show()
+    # qtbot.stop()
