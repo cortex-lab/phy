784d903b9125f1926efeae7871c80240f608de62
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-25 16:21:51 +0100

    WIP: plot function

--- phy/plot/glsl/plot.vert
+++ phy/plot/glsl/plot.vert
@@ -3,7 +3,7 @@
 attribute vec3 a_position;
 attribute float a_signal_index;  // 0..n_signals-1
 
-uniform sampler2D u_signal_colors;
+uniform sampler2D u_plot_colors;
 uniform float n_signals;
 
 varying vec4 v_color;
@@ -14,6 +14,6 @@ void main() {
     gl_Position = transform(xy);
     gl_Position.z = a_position.z;
 
-    v_color = fetch_texture(a_signal_index, u_signal_colors, n_signals);
+    v_color = fetch_texture(a_signal_index, u_plot_colors, n_signals);
     v_signal_index = a_signal_index;
 }
--- phy/plot/tests/test_plot.py
+++ phy/plot/tests/test_plot.py
@@ -10,6 +10,7 @@
 import numpy as np
 
 from ..plot import GridView
+from ..visuals import _get_linear_x
 
 
 #------------------------------------------------------------------------------
@@ -29,7 +30,7 @@ def _show(qtbot, view, stop=False):
 # Test plotting interface
 #------------------------------------------------------------------------------
 
-def test_subplot_view(qtbot):
+def test_grid_scatter(qtbot):
     view = GridView(2, 3)
     n = 1000
 
@@ -43,5 +44,18 @@ def test_subplot_view(qtbot):
     view[1, 1]
     view[1, 2].scatter(x[::5], y[::5], marker='heart',
                        color=(1, 0, 0, .25), size=20)
+
+    _show(qtbot, view)
+
+
+def test_grid_plot(qtbot):
+    view = GridView(1, 2)
+    n_plots, n_samples = 10, 50
+
+    x = _get_linear_x(n_plots, n_samples)
+    y = np.random.randn(n_plots, n_samples)
+
+    view[0, 0].plot(x, y)
+    view[0, 1].plot(x, y, color=np.random.uniform(.5, .8, size=(n_plots, 4)))
 
     _show(qtbot, view)
