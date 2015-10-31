1a4e978922cb45a20b0236f599d536f1e0c5bc44
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-31 16:45:53 +0100

    WIP: widgets

--- phy/gui/tests/test_widgets.py
+++ phy/gui/tests/test_widgets.py
@@ -13,8 +13,18 @@ from ..widgets import HTMLWidget
 # Test actions
 #------------------------------------------------------------------------------
 
-def test_widget(qtbot):
+def test_widget_empty(qtbot):
     widget = HTMLWidget()
+    widget.show()
+    qtbot.waitForWindowShown(widget)
+    # qtbot.stop()
+
+
+def test_widget_html(qtbot):
+    widget = HTMLWidget()
+    widget.add_styles('html, body, p {background-color: purple;}')
+    widget.add_header('<!-- comment -->')
+    widget.set_body('Hello world!')
     widget.show()
     qtbot.waitForWindowShown(widget)
     # qtbot.stop()
