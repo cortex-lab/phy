5081c9ae4747e45beb10da537ec3e2b67ef3ce79
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-10-31 17:22:20 +0100

    WIP: sort table

--- phy/gui/tests/test_widgets.py
+++ phy/gui/tests/test_widgets.py
@@ -6,7 +6,7 @@
 # Imports
 #------------------------------------------------------------------------------
 
-from ..widgets import HTMLWidget
+from ..widgets import HTMLWidget, Table
 
 
 #------------------------------------------------------------------------------
@@ -25,6 +25,34 @@ def test_widget_html(qtbot):
     widget.add_styles('html, body, p {background-color: purple;}')
     widget.add_header('<!-- comment -->')
     widget.set_body('Hello world!')
+
     widget.show()
     qtbot.waitForWindowShown(widget)
+    # qtbot.stop()
+
+
+def test_table(qtbot):
+    table = Table()
+
+    table.set_body("""
+
+        <table id="the-table" class="sort">
+            <thead>
+                <tr><th>id</th><th>count</th></tr>
+            </thead>
+            <tbody>
+                <tr><td>1</td><td>20</td></tr>
+                <tr><td>2</td><td>10</td></tr>
+                <tr><td>3</td><td>30</td></tr>
+            </tbody>
+        </table>
+
+        <script>
+            new Tablesort(document.getElementById('the-table'));
+        </script>
+
+    """)
+
+    table.show()
+    qtbot.waitForWindowShown(table)
     # qtbot.stop()
