0b2cd4aa7adf2851f51e69dbfcab97b21ce54816
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-11-11 21:58:24 +0100

    Rename default wizard function

--- phy/cluster/manual/__init__.py
+++ phy/cluster/manual/__init__.py
@@ -5,5 +5,5 @@
 
 from ._utils import ClusterMeta
 from .clustering import Clustering
-from .gui_component import ManualClustering
+from .gui_component import ManualClustering, default_wizard_functions
 from .views import WaveformView, TraceView, FeatureView, CorrelogramView
