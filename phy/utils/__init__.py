6cdc244ef619da201ee68d8bb5f98058a294d812
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-12-17 16:26:10 +0100

    Update GUI save state

--- phy/utils/__init__.py
+++ phy/utils/__init__.py
@@ -8,4 +8,4 @@ from ._types import (_is_array_like, _as_array, _as_tuple, _as_list,
                      Bunch, _is_list)
 from .event import EventEmitter, ProgressReporter
 from .plugin import IPlugin, get_plugin, get_all_plugins
-from .config import _ensure_dir_exists, load_master_config
+from .config import _ensure_dir_exists, load_master_config, phy_user_dir
