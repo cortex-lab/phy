4458dafbb2e9d966e2a48484f8f0e3545107090c
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-12-20 18:55:10 +0100

    Add model data in cluster store

--- phy/cluster/manual/tests/test_store.py
+++ phy/cluster/manual/tests/test_store.py
@@ -18,11 +18,21 @@ def test_create_cluster_store(model):
     selector = Selector(spike_clusters=model.spike_clusters,
                         spikes_per_cluster=model.spikes_per_cluster)
     cs = create_cluster_store(model, selector=selector)
+
+    nspk = len(model.spikes_per_cluster[1])
+
+    assert cs.masks(1).shape == (nspk, model.n_channels)
+    assert cs.features(1).shape == (nspk, model.n_channels,
+                                    model.n_features_per_channel)
+    assert cs.waveforms(1).shape == (nspk, model.n_samples_waveforms,
+                                     model.n_channels)
+
     assert cs.mean_masks(1).shape == (model.n_channels,)
     assert cs.mean_features(1).shape == (model.n_channels,
                                          model.n_features_per_channel)
     assert cs.mean_waveforms(1).shape == (model.n_samples_waveforms,
                                           model.n_channels)
+
     assert 1 <= cs.best_channels(1).shape[0] <= model.n_channels
     assert 0 < cs.max_waveform_amplitude(1) < 1
     assert cs.mean_masked_features_score(1, 2) > 0
