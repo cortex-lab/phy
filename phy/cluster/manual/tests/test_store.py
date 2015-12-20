ffa8c855ea67c1a3eed2f210bac08eb83ddba9db
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-12-20 21:06:06 +0100

    Increase cluster store coverage

--- phy/cluster/manual/tests/test_store.py
+++ phy/cluster/manual/tests/test_store.py
@@ -14,42 +14,6 @@ from phy.io import Context, Selector
 # Test cluster stats
 #------------------------------------------------------------------------------
 
-def test_create_cluster_store(model):
-    selector = Selector(spike_clusters=model.spike_clusters,
-                        spikes_per_cluster=model.spikes_per_cluster)
-    cs = create_cluster_store(model, selector=selector)
-
-    nc = model.n_channels
-    nfpc = model.n_features_per_channel
-    ns = len(model.spikes_per_cluster[1])
-    nsw = model.n_samples_waveforms
-
-    def _check(out, *shape):
-        spikes, arr = out
-        assert spikes.shape[0] == shape[0]
-        assert arr.shape == shape
-
-    def _check2(arr, *shape):
-        assert arr.shape == shape
-
-    _check(cs.masks(1), ns, nc)
-    _check(cs.features(1), ns, nc, nfpc)
-    _check(cs.waveforms(1), ns, nsw, nc)
-
-    _check(cs.features_masks(1), ns, nc * nfpc, 2)
-    spike_ids, w, m = cs.waveforms_masks(1)
-    _check((spike_ids, w), ns, nsw, nc)
-    _check((spike_ids, m), ns, nc)
-
-    assert cs.mean_masks(1).shape == (nc,)
-    assert cs.mean_features(1).shape == (nc, nfpc)
-    assert cs.mean_waveforms(1).shape == (nsw, nc)
-
-    assert 1 <= cs.best_channels(1).shape[0] <= nc
-    assert 0 < cs.max_waveform_amplitude(1) < 1
-    assert cs.mean_masked_features_score(1, 2) > 0
-
-
 def test_cluster_store(tempdir):
     context = Context(tempdir)
     cs = ClusterStore(context=context)
@@ -60,3 +24,51 @@ def test_cluster_store(tempdir):
 
     assert cs.f(3) == 9
     assert cs.f(3) == 9
+
+
+def test_create_cluster_store(model):
+    selector = Selector(spike_clusters=model.spike_clusters,
+                        spikes_per_cluster=model.spikes_per_cluster)
+    cs = create_cluster_store(model, selector=selector)
+
+    nc = model.n_channels
+    nfpc = model.n_features_per_channel
+    ns = len(model.spikes_per_cluster[1])
+    ns2 = len(model.spikes_per_cluster[2])
+    nsw = model.n_samples_waveforms
+
+    def _check(out, *shape):
+        spikes, arr = out
+        assert spikes.shape[0] == shape[0]
+        assert arr.shape == shape
+
+    def _check2(arr, *shape):
+        assert arr.shape == shape
+
+    # Model data.
+    _check(cs.masks(1), ns, nc)
+    _check(cs.features(1), ns, nc, nfpc)
+    _check(cs.waveforms(1), ns, nsw, nc)
+
+    _check(cs.features_masks(1), ns, nc * nfpc, 2)
+    spike_ids, w, m = cs.waveforms_masks(1)
+    _check((spike_ids, w), ns, nsw, nc)
+    _check((spike_ids, m), ns, nc)
+
+    # Test concat multiple clusters.
+    spike_ids, fm = cs.features_masks([1, 2])
+    assert len(spike_ids) == ns + ns2
+    assert fm.shape == (ns + ns2, nc * nfpc, 2)
+
+    # Test means.
+    assert cs.mean_masks(1).shape == (nc,)
+    assert cs.mean_features(1).shape == (nc, nfpc)
+    assert cs.mean_waveforms(1).shape == (nsw, nc)
+
+    assert 0 < cs.waveform_lim() < 1
+
+    # Statistics.
+    assert 1 <= len(cs.best_channels(1)) <= nc
+    assert 1 <= len(cs.best_channels_multiple([1, 2])) <= nc
+    assert 0 < cs.max_waveform_amplitude(1) < 1
+    assert cs.mean_masked_features_score(1, 2) > 0
