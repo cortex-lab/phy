Fix floating-point precision issue in plot: convert to float32 at the last moment

phy/plot/plot.py
@@ -139,7 +139,7 @@ class View(BaseCanvas):
             # so we can replace this with `if 'a_box_index' in visual.program`
             # after the next VisPy release.
             if 'a_box_index' in visual.program._code_variables:
-                visual.program['a_box_index'] = box_index
+                visual.program['a_box_index'] = box_index.astype(np.float32)
         self.update()
 
     @contextmanager

phy/plot/tests/test_base.py
@@ -119,8 +119,9 @@ def test_visual_2(qtbot, canvas, vertex_shader, fragment_shader):
             self.inserter.insert_vert(s, 'after_transforms')
 
         def set_data(self):
-            data = np.random.uniform(0, 20, (1000, 2)).astype(np.float32)
-            self.program['a_position'] = self.transforms.apply(data)
+            data = np.random.uniform(0, 20, (1000, 2))
+            pos = self.transforms.apply(data).astype(np.float32)
+            self.program['a_position'] = pos
 
     bounds = subplot_bounds(shape=(2, 3), index=(1, 2))
     canvas.transforms.add_on_gpu([Subplot((2, 3), (1, 2)),

phy/plot/tests/test_transform.py
@@ -26,12 +26,11 @@ from ..transform import (_glslify, pixels_to_ndc,
 def _check_forward(transform, array, expected):
     transformed = transform.apply(array)
     if array is None or not len(array):
-        assert transformed == array
+        assert transformed is None or not len(transformed)
         return
     array = np.atleast_2d(array)
     if isinstance(array, np.ndarray):
         assert transformed.shape[1] == array.shape[1]
-        assert transformed.dtype == np.float32
     if not len(transformed):
         assert not len(expected)
     else:
@@ -39,6 +38,8 @@ def _check_forward(transform, array, expected):
 
 
 def _check(transform, array, expected):
+    array = np.array(array, dtype=np.float64)
+    expected = np.array(expected, dtype=np.float64)
     _check_forward(transform, array, expected)
     # Test the inverse transform if it is implemented.
     try:
@@ -176,7 +177,7 @@ def test_subplot_glsl():
 
 @yield_fixture
 def array():
-    yield np.array([[-1, 0], [1, 2]])
+    yield np.array([[-1., 0.], [1., 2.]])
 
 
 def test_transform_chain_empty(array):
@@ -234,14 +235,14 @@ def test_transform_chain_add():
     tc.add_on_cpu([Scale(.5)])
 
     tc_2 = TransformChain()
-    tc_2.add_on_cpu([Scale(2)])
+    tc_2.add_on_cpu([Scale(2.)])
 
-    ae((tc + tc_2).apply([3]), [[3]])
+    ae((tc + tc_2).apply([3.]), [[3.]])
 
 
 def test_transform_chain_inverse():
     tc = TransformChain()
     tc.add_on_cpu([Scale(.5), Translate((1, 0)), Scale(2)])
     tci = tc.inverse()
-    ae(tc.apply([[1, 0]]), [[3, 0]])
-    ae(tci.apply([[3, 0]]), [[1, 0]])
+    ae(tc.apply([[1., 0.]]), [[3., 0.]])
+    ae(tci.apply([[3., 0.]]), [[1., 0.]])

phy/plot/transform.py
@@ -26,10 +26,10 @@ def _wrap_apply(f):
         if arr is None or not len(arr):
             return arr
         arr = np.atleast_2d(arr)
-        arr = arr.astype(np.float32)
         assert arr.ndim == 2
+        assert arr.dtype == np.float64
         out = f(arr, **kwargs)
-        out = out.astype(np.float32)
+        assert out.dtype == np.float64
         out = np.atleast_2d(out)
         assert out.ndim == 2
         assert out.shape[1] == arr.shape[1]
@@ -98,8 +98,8 @@ def subplot_bounds_glsl(shape=None, index=None):
 
 def pixels_to_ndc(pos, size=None):
     """Convert from pixels to normalized device coordinates (in [-1, 1])."""
-    pos = np.asarray(pos, dtype=np.float32)
-    size = np.asarray(size, dtype=np.float32)
+    pos = np.asarray(pos, dtype=np.float64)
+    size = np.asarray(size, dtype=np.float64)
     pos = pos / (size / 2.) - 1
     # Flip y, because the origin in pixels is at the top left corner of the
     # window.

phy/plot/visuals.py
@@ -107,9 +107,10 @@ class ScatterVisual(BaseVisual):
         data = self.validate(*args, **kwargs)
         self.data_range.from_bounds = data.data_bounds
         pos_tr = self.transforms.apply(data.pos)
-        self.program['a_position'] = np.c_[pos_tr, data.depth]
-        self.program['a_size'] = data.size
-        self.program['a_color'] = data.color
+        pos_tr = np.c_[pos_tr, data.depth]
+        self.program['a_position'] = pos_tr.astype(np.float32)
+        self.program['a_size'] = data.size.astype(np.float32)
+        self.program['a_color'] = data.color.astype(np.float32)
 
 
 def _as_list(arr):
@@ -179,7 +180,7 @@ class PlotVisual(BaseVisual):
         assert depth.shape == (n_signals, 1)
 
         data_bounds = _get_data_bounds(data_bounds, length=n_signals)
-        data_bounds = data_bounds.astype(np.float32)
+        data_bounds = data_bounds.astype(np.float64)
         assert data_bounds.shape == (n_signals, 4)
 
         return Bunch(x=x, y=y,
@@ -202,7 +203,7 @@ class PlotVisual(BaseVisual):
         y = np.concatenate(data.y) if len(data.y) else np.array([])
 
         # Generate the position array.
-        pos = np.empty((n, 2), dtype=np.float32)
+        pos = np.empty((n, 2), dtype=np.float64)
         pos[:, 0] = x.ravel()
         pos[:, 1] = y.ravel()
         assert pos.shape == (n, 2)
@@ -215,7 +216,7 @@ class PlotVisual(BaseVisual):
 
         # Generate signal index.
         signal_index = np.repeat(np.arange(n_signals), n_samples)
-        signal_index = _get_array(signal_index, (n, 1)).astype(np.float32)
+        signal_index = _get_array(signal_index, (n, 1))
         assert signal_index.shape == (n, 1)
 
         # Transform the positions.
@@ -225,10 +226,9 @@ class PlotVisual(BaseVisual):
 
         # Position and depth.
         depth = np.repeat(data.depth, n_samples, axis=0)
-        self.program['a_position'] = np.c_[pos_tr, depth]
-        self.program['a_color'] = color
-        self.program['a_signal_index'] = signal_index
-        # self.program['n_signals'] = n_signals
+        self.program['a_position'] = np.c_[pos_tr, depth].astype(np.float32)
+        self.program['a_color'] = color.astype(np.float32)
+        self.program['a_signal_index'] = signal_index.astype(np.float32)
 
 
 class HistogramVisual(BaseVisual):
@@ -248,7 +248,7 @@ class HistogramVisual(BaseVisual):
                  color=None,
                  ylim=None):
         assert hist is not None
-        hist = np.asarray(hist, np.float32)
+        hist = np.asarray(hist, np.float64)
         if hist.ndim == 1:
             hist = hist[None, :]
         assert hist.ndim == 2
@@ -295,18 +295,17 @@ class HistogramVisual(BaseVisual):
 
         # Set the transformed position.
         pos = np.vstack(_tesselate_histogram(row) for row in hist)
-        pos = pos.astype(np.float32)
         pos_tr = self.transforms.apply(pos)
         assert pos_tr.shape == (n, 2)
-        self.program['a_position'] = pos_tr
+        self.program['a_position'] = pos_tr.astype(np.float32)
 
         # Generate the hist index.
-        self.program['a_hist_index'] = _get_index(n_hists, n_bins * 6, n)
+        hist_index = _get_index(n_hists, n_bins * 6, n)
+        self.program['a_hist_index'] = hist_index.astype(np.float32)
 
         # Hist colors.
-        self.program['u_color'] = _get_texture(data.color,
-                                               self._default_color,
-                                               n_hists, [0, 1])
+        tex = _get_texture(data.color, self._default_color, n_hists, [0, 1])
+        self.program['u_color'] = tex.astype(np.float32)
         self.program['n_hists'] = n_hists
 
 
@@ -343,7 +342,6 @@ class LineVisual(BaseVisual):
         assert pos.ndim == 2
         n_lines = pos.shape[0]
         assert pos.shape[1] == 4
-        pos = pos.astype(np.float32)
 
         # Color.
         color = _get_array(color, (n_lines, 4), LineVisual._default_color)
@@ -352,7 +350,7 @@ class LineVisual(BaseVisual):
         if data_bounds is None:
             data_bounds = NDC
         data_bounds = _get_data_bounds(data_bounds, length=n_lines)
-        data_bounds = data_bounds.astype(np.float32)
+        data_bounds = data_bounds.astype(np.float64)
         assert data_bounds.shape == (n_lines, 4)
 
         return Bunch(pos=pos, color=color, data_bounds=data_bounds)
@@ -367,7 +365,7 @@ class LineVisual(BaseVisual):
         pos = data.pos
         assert pos.ndim == 2
         assert pos.shape[1] == 4
-        assert pos.dtype == np.float32
+        assert pos.dtype == np.float64
         n_lines = pos.shape[0]
         n_vertices = 2 * n_lines
         pos = pos.reshape((-1, 2))
@@ -379,8 +377,8 @@ class LineVisual(BaseVisual):
 
         # Position.
         assert pos_tr.shape == (n_vertices, 2)
-        self.program['a_position'] = pos_tr
+        self.program['a_position'] = pos_tr.astype(np.float32)
 
         # Color.
-        color = np.repeat(data.color, 2, axis=0).astype(np.float32)
-        self.program['a_color'] = color
+        color = np.repeat(data.color, 2, axis=0)
+        self.program['a_color'] = color.astype(np.float32)