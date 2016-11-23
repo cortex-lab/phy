9c4039cf5284963307835d219ae3a18cf7de9502
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-11-23 17:27:24 +0100

    Refactor correlogram view

--- phy/cluster/views/tests/test_correlogram.py
+++ phy/cluster/views/tests/test_correlogram.py
@@ -6,16 +6,38 @@
 # Imports
 #------------------------------------------------------------------------------
 
-from .conftest import _select_clusters
+from phy.gui import GUI
+from phy.io.mock import (artificial_correlograms,
+                         )
+
+from ..correlogram import CorrelogramView
 
 
 #------------------------------------------------------------------------------
 # Test correlogram view
 #------------------------------------------------------------------------------
 
-def test_correlogram_view(qtbot, gui):
-    v = gui.controller.add_correlogram_view(gui)
-    _select_clusters(gui)
+def test_correlogram_view(qtbot):
+
+    nc = 5
+    ns = 50
+
+    def get_correlograms(cluster_id, bin_size, window_size):
+        return artificial_correlograms(nc, ns)
+
+    v = CorrelogramView(correlograms=get_correlograms,
+                        sample_rate=100.,
+                        )
+    gui = GUI()
+    gui.show()
+    v.attach(gui)
+
+    # qtbot.waitForWindowShown(gui)
+
+    v.on_select([])
+    v.on_select([0])
+    v.on_select([0, 2, 3])
+    v.on_select([0, 2])
 
     v.toggle_normalization()
 
