3c137123b1da4287d3546fa6b5a4ec90768bb68e
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-11-30 12:35:27 +0100

    Refactor scatter view

--- phy/cluster/views/scatter.py
+++ phy/cluster/views/scatter.py
@@ -13,6 +13,7 @@ import numpy as np
 
 from phy.utils._color import _colormap
 from .base import ManualClusteringView
+from phy.plot import NDC
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +35,33 @@ class ScatterView(ManualClusteringView):
         # Initialize the view.
         super(ScatterView, self).__init__(**kwargs)
 
+    def _get_data(self, cluster_ids):
+        return [self.coords(cluster_id) for cluster_id in cluster_ids]
+
+    def _get_data_bounds(self, bunchs):
+        if not bunchs:
+            return NDC
+        data_bounds = bunchs[0].get('data_bounds', None)
+        if data_bounds is None:
+            xmin = np.min([d.x.min() for d in bunchs])
+            ymin = np.min([d.y.min() for d in bunchs])
+            xmax = np.max([d.x.max() for d in bunchs])
+            ymax = np.max([d.y.max() for d in bunchs])
+            data_bounds = (xmin, ymin, xmax, ymax)
+        return data_bounds
+
+    def _plot_points(self, bunchs, data_bounds):
+        for i, d in enumerate(bunchs):
+            x, y = d.x, d.y
+            assert x.ndim == y.ndim == 1
+            assert x.shape == y.shape
+
+            self.scatter(x=x, y=y,
+                         color=tuple(_colormap(i)) + (.5,),
+                         size=self._default_marker_size,
+                         data_bounds=data_bounds,
+                         )
+
     def on_select(self, cluster_ids=None):
         super(ScatterView, self).on_select(cluster_ids)
         cluster_ids = self.cluster_ids
@@ -42,28 +70,11 @@ class ScatterView(ManualClusteringView):
             return
 
         # Retrieve the data.
-        ds = [self.coords(cluster_id) for cluster_id in cluster_ids]
+        bunchs = self._get_data(cluster_ids)
 
         # Compute the data bounds.
-        data_bounds = ds[0].get('data_bounds', None)
-        if data_bounds is None:
-            xmin = np.min([d.x.min() for d in ds])
-            ymin = np.min([d.y.min() for d in ds])
-            xmax = np.max([d.x.max() for d in ds])
-            ymax = np.max([d.y.max() for d in ds])
-            data_bounds = (xmin, ymin, xmax, ymax)
+        data_bounds = self._get_data_bounds(bunchs)
 
         # Plot the points.
         with self.building():
-            for i, cluster_id in enumerate(ds):
-                d = ds[i]
-                x = d.x
-                y = d.y
-                assert x.ndim == y.ndim == 1
-                assert x.shape == y.shape
-
-                self.scatter(x=x, y=y,
-                             color=tuple(_colormap(i)) + (.5,),
-                             size=self._default_marker_size,
-                             data_bounds=data_bounds,
-                             )
+            self._plot_points(bunchs, data_bounds)
