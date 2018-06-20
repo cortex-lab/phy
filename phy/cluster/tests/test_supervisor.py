40c2d9c54c6e151f161323d755217e299d64f9b4
Cyrille Rossant <rossant@users.noreply.github.com>
2018-06-20 15:52:57 +0200

    WIP: refactor supervisor tests

--- phy/cluster/tests/test_supervisor.py
+++ phy/cluster/tests/test_supervisor.py
@@ -239,129 +239,124 @@ def test_supervisor_order(qtbot, supervisor):
 
 
 def test_supervisor_edge_cases(supervisor):
-    mc = supervisor
 
     # Empty selection at first.
-    ae(mc.clustering.cluster_ids, [0, 1, 2, 10, 11, 20, 30])
+    ae(supervisor.clustering.cluster_ids, [0, 1, 2, 10, 11, 20, 30])
 
-    mc.select([0])
-    assert mc.selected == [0]
+    supervisor.select([0])
+    _assert_selected(supervisor, [0])
 
-    mc.undo()
-    mc.redo()
+    supervisor.undo()
+    supervisor.redo()
 
     # Merge.
-    mc.merge()
-    assert mc.selected == [0]
+    supervisor.merge()
+    _assert_selected(supervisor, [0])
 
-    mc.merge([])
-    assert mc.selected == [0]
+    supervisor.merge([])
+    _assert_selected(supervisor, [0])
 
-    mc.merge([10])
-    assert mc.selected == [0]
+    supervisor.merge([10])
+    _assert_selected(supervisor, [0])
 
     # Split.
-    mc.split([])
-    assert mc.selected == [0]
+    supervisor.split([])
+    _assert_selected(supervisor, [0])
 
     # Move.
-    mc.move('ignored', [])
+    supervisor.move('ignored', [])
 
-    mc.save()
+    supervisor.save()
 
 
 def test_supervisor_skip(qtbot, gui, supervisor):
-    mc = supervisor
 
     # yield [0, 1, 2, 10, 11, 20, 30]
     # #      i, g, N,  i,  g,  N, N
     expected = [30, 20, 11, 2, 1]
 
     for clu in expected:
-        mc.cluster_view.next()
-        assert mc.selected == [clu]
+        supervisor.cluster_view.next()
+        _assert_selected(supervisor, [clu])
 
 
 def test_supervisor_merge(supervisor):
-    mc = supervisor
 
-    mc.cluster_view.select([30])
-    mc.similarity_view.select([20])
-    assert mc.selected == [30, 20]
+    supervisor.cluster_view.select([30])
+    supervisor.similarity_view.select([20])
+    _assert_selected(supervisor, [30, 20])
 
-    mc.merge()
-    assert mc.selected == [31, 11]
+    supervisor.merge()
+    _assert_selected(supervisor, [31, 11])
 
-    mc.undo()
-    assert mc.selected == [30, 20]
+    supervisor.undo()
+    _assert_selected(supervisor, [30, 20])
 
-    mc.redo()
-    assert mc.selected == [31, 11]
+    supervisor.redo()
+    _assert_selected(supervisor, [31, 11])
 
 
 def test_supervisor_merge_move(supervisor):
     """Check that merge then move selects the next cluster in the original
     cluster view, not the updated cluster view."""
-    mc = supervisor
 
-    mc.cluster_view.select([20, 11])
+    supervisor.cluster_view.select([20, 11])
 
-    mc.merge()
-    assert mc.selected == [31]
+    supervisor.merge()
+    _assert_selected(supervisor, [31])
 
-    mc.move('good')
-    assert mc.selected == [2]
+    supervisor.move('good')
+    _assert_selected(supervisor, [2])
 
-    mc.cluster_view.select([30])
+    supervisor.cluster_view.select([30])
 
-    mc.move('good')
-    assert mc.selected == [2]
+    supervisor.move('good')
+    _assert_selected(supervisor, [2])
 
 
 def test_supervisor_split_0(supervisor):
-    mc = supervisor
 
-    mc.select([1, 2])
-    mc.split([1, 2])
-    assert mc.selected == [31]
+    supervisor.select([1, 2])
+    supervisor.split([1, 2])
+    _assert_selected(supervisor, [31])
 
-    mc.undo()
-    assert mc.selected == [1, 2]
+    supervisor.undo()
+    _assert_selected(supervisor, [1, 2])
 
-    mc.redo()
-    assert mc.selected == [31]
+    supervisor.redo()
+    _assert_selected(supervisor, [31])
 
 
 def test_supervisor_split_1(gui, supervisor):
-    mc = supervisor
-    mc.select([1, 2])
+
+    supervisor.select([1, 2])
 
     @gui.connect_
     def on_request_split():
-        return mc.clustering.spikes_in_clusters([1, 2])
+        return supervisor.clustering.spikes_in_clusters([1, 2])
 
-    mc.split()
-    assert mc.selected == [31]
+    supervisor.split()
+    _assert_selected(supervisor, [31])
 
 
 def test_supervisor_split_2(gui, quality, similarity):
     spike_clusters = np.array([0, 0, 1])
 
-    mc = Supervisor(spike_clusters,
-                    similarity=similarity,
-                    )
-    mc.attach(gui)
+    supervisor = Supervisor(spike_clusters,
+                            similarity=similarity,
+                            )
+    supervisor.attach(gui)
 
-    mc.add_column(quality, name='quality', default=True)
-    mc.set_default_sort('quality', 'desc')
+    supervisor.add_column(quality, name='quality', default=True)
+    supervisor.set_default_sort('quality', 'desc')
 
-    mc.split([0])
-    assert mc.selected == [3, 2]
+    supervisor.split([0])
+    _assert_selected(supervisor, [3, 2])
 
 
 def test_supervisor_state(tempdir, qtbot, gui, supervisor):
-    mc = supervisor
-    cv = mc.cluster_view
+
+    cv = supervisor.cluster_view
     cv.sort_by('id')
     gui.close()
     assert cv.state['sort_by'] == ('id', 'asc')
@@ -370,51 +365,48 @@ def test_supervisor_state(tempdir, qtbot, gui, supervisor):
 
 
 def test_supervisor_label(supervisor):
-    mc = supervisor
 
-    mc.select([20])
-    mc.label("my_field", 3.14)
+    supervisor.select([20])
+    supervisor.label("my_field", 3.14)
 
-    mc.save()
+    supervisor.save()
 
-    assert 'my_field' in mc.fields
-    assert mc.get_labels('my_field')[20] == 3.14
+    assert 'my_field' in supervisor.fields
+    assert supervisor.get_labels('my_field')[20] == 3.14
 
 
 def test_supervisor_move_1(supervisor):
-    mc = supervisor
 
-    mc.select([20])
-    assert mc.selected == [20]
+    supervisor.select([20])
+    _assert_selected(supervisor, [20])
 
-    assert not mc.move('', '')
+    assert not supervisor.move('', '')
 
-    mc.move('noise')
-    assert mc.selected == [11]
+    supervisor.move('noise')
+    _assert_selected(supervisor, [11])
 
-    mc.undo()
-    assert mc.selected == [20]
+    supervisor.undo()
+    _assert_selected(supervisor, [20])
 
-    mc.redo()
-    assert mc.selected == [11]
+    supervisor.redo()
+    _assert_selected(supervisor, [11])
 
 
 def test_supervisor_move_2(supervisor):
-    mc = supervisor
 
-    mc.select([20])
-    mc.similarity_view.select([10])
+    supervisor.select([20])
+    supervisor.similarity_view.select([10])
 
-    assert mc.selected == [20, 10]
+    _assert_selected(supervisor, [20, 10])
 
-    mc.move('noise', 10)
-    assert mc.selected == [20, 2]
+    supervisor.move('noise', 10)
+    _assert_selected(supervisor, [20, 2])
 
-    mc.undo()
-    assert mc.selected == [20, 10]
+    supervisor.undo()
+    _assert_selected(supervisor, [20, 10])
 
-    mc.redo()
-    assert mc.selected == [20, 2]
+    supervisor.redo()
+    _assert_selected(supervisor, [20, 2])
 
 
 #------------------------------------------------------------------------------
@@ -422,104 +414,99 @@ def test_supervisor_move_2(supervisor):
 #------------------------------------------------------------------------------
 
 def test_supervisor_action_reset(qtbot, supervisor):
-    mc = supervisor
 
-    mc.actions.select([10, 11])
+    supervisor.actions.select([10, 11])
 
-    mc.actions.reset()
-    assert mc.selected == [30]
+    supervisor.actions.reset()
+    _assert_selected(supervisor, [30])
 
-    mc.actions.next()
-    assert mc.selected == [30, 20]
+    supervisor.actions.next()
+    _assert_selected(supervisor, [30, 20])
 
-    mc.actions.next()
-    assert mc.selected == [30, 11]
+    supervisor.actions.next()
+    _assert_selected(supervisor, [30, 11])
 
-    mc.actions.previous()
-    assert mc.selected == [30, 20]
+    supervisor.actions.previous()
+    _assert_selected(supervisor, [30, 20])
 
 
 def test_supervisor_action_nav(qtbot, supervisor):
-    mc = supervisor
 
-    mc.actions.reset()
-    assert mc.selected == [30]
+    supervisor.actions.reset()
+    _assert_selected(supervisor, [30])
 
-    mc.actions.next_best()
-    assert mc.selected == [20]
+    supervisor.actions.next_best()
+    _assert_selected(supervisor, [20])
 
-    mc.actions.previous_best()
-    assert mc.selected == [30]
+    supervisor.actions.previous_best()
+    _assert_selected(supervisor, [30])
 
 
 def test_supervisor_action_move_1(qtbot, supervisor):
-    mc = supervisor
 
-    mc.actions.next()
+    supervisor.actions.next()
 
-    assert mc.selected == [30]
-    mc.actions.move_best_to_noise()
+    _assert_selected(supervisor, [30])
+    supervisor.actions.move_best_to_noise()
 
-    assert mc.selected == [20]
-    mc.actions.move_best_to_mua()
+    _assert_selected(supervisor, [20])
+    supervisor.actions.move_best_to_mua()
 
-    assert mc.selected == [11]
-    mc.actions.move_best_to_good()
+    _assert_selected(supervisor, [11])
+    supervisor.actions.move_best_to_good()
 
-    assert mc.selected == [2]
+    _assert_selected(supervisor, [2])
 
-    mc.cluster_meta.get('group', 30) == 'noise'
-    mc.cluster_meta.get('group', 20) == 'mua'
-    mc.cluster_meta.get('group', 11) == 'good'
+    supervisor.cluster_meta.get('group', 30) == 'noise'
+    supervisor.cluster_meta.get('group', 20) == 'mua'
+    supervisor.cluster_meta.get('group', 11) == 'good'
 
     # qtbot.stop()
 
 
 def test_supervisor_action_move_2(supervisor):
-    mc = supervisor
 
-    mc.select([30])
-    mc.similarity_view.select([20])
+    supervisor.select([30])
+    supervisor.similarity_view.select([20])
 
-    assert mc.selected == [30, 20]
-    mc.actions.move_similar_to_noise()
+    _assert_selected(supervisor, [30, 20])
+    supervisor.actions.move_similar_to_noise()
 
-    assert mc.selected == [30, 11]
-    mc.actions.move_similar_to_mua()
+    _assert_selected(supervisor, [30, 11])
+    supervisor.actions.move_similar_to_mua()
 
-    assert mc.selected == [30, 2]
-    mc.actions.move_similar_to_good()
+    _assert_selected(supervisor, [30, 2])
+    supervisor.actions.move_similar_to_good()
 
-    assert mc.selected == [30, 1]
+    _assert_selected(supervisor, [30, 1])
 
-    mc.cluster_meta.get('group', 20) == 'noise'
-    mc.cluster_meta.get('group', 11) == 'mua'
-    mc.cluster_meta.get('group', 2) == 'good'
+    supervisor.cluster_meta.get('group', 20) == 'noise'
+    supervisor.cluster_meta.get('group', 11) == 'mua'
+    supervisor.cluster_meta.get('group', 2) == 'good'
 
 
 def test_supervisor_action_move_3(supervisor):
-    mc = supervisor
 
-    mc.select([30])
-    mc.similarity_view.select([20])
+    supervisor.select([30])
+    supervisor.similarity_view.select([20])
 
-    assert mc.selected == [30, 20]
-    mc.actions.move_all_to_noise()
-    mc.next()
+    _assert_selected(supervisor, [30, 20])
+    supervisor.actions.move_all_to_noise()
+    supervisor.next()
 
-    assert mc.selected == [11, 2]
-    mc.actions.move_all_to_mua()
+    _assert_selected(supervisor, [11, 2])
+    supervisor.actions.move_all_to_mua()
 
-    assert mc.selected == [1]
-    mc.actions.move_all_to_good()
+    _assert_selected(supervisor, [1])
+    supervisor.actions.move_all_to_good()
 
-    assert mc.selected == [1]
+    _assert_selected(supervisor, [1])
 
-    mc.cluster_meta.get('group', 30) == 'noise'
-    mc.cluster_meta.get('group', 20) == 'noise'
+    supervisor.cluster_meta.get('group', 30) == 'noise'
+    supervisor.cluster_meta.get('group', 20) == 'noise'
 
-    mc.cluster_meta.get('group', 11) == 'mua'
-    mc.cluster_meta.get('group', 10) == 'mua'
+    supervisor.cluster_meta.get('group', 11) == 'mua'
+    supervisor.cluster_meta.get('group', 10) == 'mua'
 
-    mc.cluster_meta.get('group', 2) == 'good'
-    mc.cluster_meta.get('group', 1) == 'good'
+    supervisor.cluster_meta.get('group', 2) == 'good'
+    supervisor.cluster_meta.get('group', 1) == 'good'
