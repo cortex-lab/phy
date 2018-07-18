d59622f18efee814aa86ec50d3c33306ca8b2013
Cyrille Rossant <rossant@users.noreply.github.com>
2018-07-18 13:09:33 +0200

    Fix probe view

--- phy/cluster/views/probe.py
+++ phy/cluster/views/probe.py
@@ -26,14 +26,17 @@ class ProbeView(HTMLWidget):
         self.positions = positions
         self.best_channels = best_channels
 
-    def on_select(self, cluster_ids=(), **kwargs):
+    def on_select(self, sender=None, cluster_ids=(), **kwargs):
         if not cluster_ids:
-            return
-        cluster_channels = {i: self.best_channels(cl)
-                            for i, cl in enumerate(cluster_ids)}
+            cluster_channels = {0: list(range(len(self.positions)))}
+        else:
+            cluster_channels = {i: self.best_channels(cl)
+                                for i, cl in enumerate(cluster_ids)}
         self.builder.set_body(probe_layout(self.positions, cluster_channels))
+        self.build()
 
     def attach(self, gui):
-        connect(self.on_select, sender=gui)
+        connect(self.on_select)
+        self.on_select()
         self.show()
         gui.add_view(self)
