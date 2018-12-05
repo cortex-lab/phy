298b30507f979c181cae6e05152a714a64ce56a2
Cyrille Rossant <rossant@users.noreply.github.com>
2018-12-05 17:52:09 +0100

    Update plot tests

--- phy/plot/tests/test_plot.py
+++ phy/plot/tests/test_plot.py
@@ -49,15 +49,18 @@ def test_plot_1(qtbot, x, y):
 
 
 #------------------------------------------------------------------------------
-# Test visuals in grid
+# Test visuals in PlotCanvas
 #------------------------------------------------------------------------------
 
-def test_grid_scatter(qtbot, x, y):
+def test_plot_grid(qtbot, x, y):
     c = PlotCanvas(layout='grid', shape=(2, 3))
 
-    c[0, 1].add(visuals.ScatterVisual(), x=x, y=y)
-    c[1, 0].add(visuals.ScatterVisual(), x, y, size=np.random.uniform(5, 20, size=1000))
+    c[0, 0].add(visuals.PlotVisual(), x=x, y=y)
+    c[0, 1].add(visuals.HistogramVisual(), 5 + x)
     c[0, 2].add(visuals.ScatterVisual(), x, y, color=np.random.uniform(.5, .8, size=(1000, 4)))
+
+    c[1, 0].add(visuals.LineVisual(), pos=[-1, -5, +1, -.5])
+    c[1, 1].add(visuals.TextVisual(), pos=(0, 0), text='Hello world!', anchor=(0., 0.))
 
     # Multiple scatters in the same subplot.
     c[1, 2].add(visuals.ScatterVisual(marker='asterisk'),
@@ -73,148 +76,48 @@ def test_grid_scatter(qtbot, x, y):
     c.close()
 
 
-def test_grid_plot(qtbot):
-    view = View(layout='grid', shape=(1, 2))
-    n_plots, n_samples = 5, 50
-
-    x = _get_linear_x(n_plots, n_samples)
-    y = np.random.randn(n_plots, n_samples)
-
-    view[0, 0].plot(x, y)
-    view[0, 1].plot(x, y, color=np.random.uniform(.5, .8, size=(n_plots, 4)))
-
-    _show(qtbot, view)
-
-
-def test_grid_plot_uniform(qtbot):
-    view = View(layout='grid', shape=(1, 2))
-    n_plots, n_samples = 5, 50
-
-    x = _get_linear_x(n_plots, n_samples)
-    y = np.random.randn(n_plots, n_samples)
-
-    view[0, 0].uplot(x, y)
-    view[0, 1].uplot(x, y, color=(1., 1., 0., .5))
-
-    _show(qtbot, view)
-
-
-def test_grid_hist(qtbot):
-    view = View(layout='grid', shape=(3, 3))
-
-    hist = np.random.rand(3, 3, 20)
-
-    for i in range(3):
-        for j in range(3):
-            view[i, j].hist(hist[i, j, :],
-                            color=np.random.uniform(.5, .8, size=4))
-
-    _show(qtbot, view)
-
-
-def test_grid_lines(qtbot):
-    view = View(layout='grid', shape=(1, 2))
-
-    view[0, 0].lines(pos=[-1, -.5, +1, -.5])
-    view[0, 1].lines(pos=[-1, +.5, +1, +.5])
-
-    _show(qtbot, view)
-
-
-def test_grid_text(qtbot):
-    view = View(layout='grid', shape=(2, 1))
-
-    view[0, 0].text(pos=(0, 0), text='Hello world!', anchor=(0., 0.))
-    view[1, 0].text(pos=[[-.5, 0], [+.5, 0]], text=['|', ':)'])
-
-    _show(qtbot, view)
-
-
-def test_grid_complete(qtbot):
-    view = View(layout='grid', shape=(2, 2))
+def test_plot_stacked(qtbot):
+    c = PlotCanvas(layout='stacked', n_plots=3)
     t = _get_linear_x(1, 1000).ravel()
 
-    view[0, 0].scatter(*np.random.randn(2, 100))
-    view[0, 1].plot(t, np.sin(20 * t), color=(1, 0, 0, 1))
+    c[0].add(visuals.ScatterVisual(), pos=np.random.rand(100, 2))
 
-    view[1, 1].hist(np.random.rand(5, 10),
-                    color=np.random.uniform(.4, .9, size=(5, 4)))
+    c[1].add(visuals.HistogramVisual(), np.random.rand(5, 10),
+             color=np.random.uniform(.4, .9, size=(5, 4)))
 
-    _show(qtbot, view)
+    c[2].add(visuals.PlotVisual(), t, np.sin(20 * t), color=(1, 0, 0, 1))
 
-
-#------------------------------------------------------------------------------
-# Test other interact
-#------------------------------------------------------------------------------
-
-def test_stacked_complete(qtbot):
-    view = View(layout='stacked', n_plots=3)
-
-    t = _get_linear_x(1, 1000).ravel()
-    view[0].scatter(*np.random.randn(2, 100))
-
-    # Different types of visuals in the same subplot.
-    view[1].hist(np.random.rand(5, 10),
-                 color=np.random.uniform(.4, .9, size=(5, 4)))
-    view[1].plot(t, np.sin(20 * t), color=(1, 0, 0, 1))
-
-    # TODO
-    # v = view[2].plot(t[::2], np.sin(20 * t[::2]), color=(1, 0, 0, 1))
-    # v.update(color=(0, 1, 0, 1))
-
-    _show(qtbot, view)
+    c.show()
+    qtbot.waitForWindowShown(c)
+    # qtbot.stop()
+    c.close()
 
 
-def test_boxed_complete(qtbot):
+def test_plot_boxed(qtbot):
     n = 3
     b = np.zeros((n, 4))
     b[:, 0] = b[:, 1] = np.linspace(-1., 1. - 2. / 3., n)
     b[:, 2] = b[:, 3] = np.linspace(-1. + 2. / 3., 1., n)
-    view = View(layout='boxed', box_bounds=b)
+    c = PlotCanvas(layout='boxed', box_bounds=b)
 
     t = _get_linear_x(1, 1000).ravel()
-    view[0].scatter(*np.random.randn(2, 100))
-    view[1].plot(t, np.sin(20 * t), color=(1, 0, 0, 1))
-    view[2].hist(np.random.rand(5, 10),
-                 color=np.random.uniform(.4, .9, size=(5, 4)))
+    c[0].add(visuals.ScatterVisual(), pos=np.random.rand(100, 2))
+    c[1].add(visuals.PlotVisual(), t, np.sin(20 * t), color=(1, 0, 0, 1))
+    c[2].add(visuals.HistogramVisual(), np.random.rand(5, 10),
+             color=np.random.uniform(.4, .9, size=(5, 4)))
 
-    _show(qtbot, view)
+    c.show()
+    qtbot.waitForWindowShown(c)
+    # qtbot.stop()
+    c.close()
 
 
 #------------------------------------------------------------------------------
 # Test lasso
 #------------------------------------------------------------------------------
 
-def _test_lasso_simple(qtbot):
-    view = View(enable_lasso=True)
-    n = 1000
-
-    x = np.random.randn(n)
-    y = np.random.randn(n)
-
-    view.scatter(x, y)
-
-    l = view.lasso
-    assert str(l)
-    # TODO
-    #ev.mouse_press(pos=(0, 0), button=1, modifiers=('Control',))
-    l.add((+1, -1))
-    l.add((+1, +1))
-    l.add((-1, +1))
-    assert l.count == 4
-    assert l.polygon.shape == (4, 2)
-    b = [[-1, -1], [+1, -1], [+1, +1], [-1, +1]]
-    ae(l.in_polygon(b), [False, False, True, True])
-
-    #ev.mouse_press(pos=(0, 0), button=2, modifiers=('Control',))
-    assert l.count == 0
-
-    _show(qtbot, view)
-
-
 def _test_lasso_grid(qtbot):
-    view = View(layout='grid', shape=(1, 2),
-                enable_lasso=True)
+    view = PlotCanvas(layout='grid', shape=(1, 2), enable_lasso=True)
     x, y = np.meshgrid(np.linspace(-1., 1., 20), np.linspace(-1., 1., 20))
     x, y = x.ravel(), y.ravel()
     view[0, 1].scatter(x, y, data_bounds=NDC)
@@ -247,5 +150,3 @@ def _test_lasso_grid(qtbot):
     ind = l.in_polygon(np.c_[x, y])
     view[0, 1].scatter(x[ind], y[ind], color=(1., 0., 0., 1.),
                        data_bounds=NDC)
-
-    _show(qtbot, view)
