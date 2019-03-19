0183d076a26da49124ff269f9ae8ec12ea01c437
Cyrille Rossant <rossant@users.noreply.github.com>
2019-03-19 15:38:30 +0100

    Fix matplotlib view test

--- phy/cluster/views/tests/test_base.py
+++ phy/cluster/views/tests/test_base.py
@@ -19,8 +19,9 @@ from ..base import ManualClusteringViewMatplotlib, zoom_fun
 
 class MyMatplotlibView(ManualClusteringViewMatplotlib):
     def on_select(self, cluster_ids, **kwargs):
-        ax = self.subplots(1, 1)[0, 0]
-        ax.plot(np.random.randn(100))
+        axes = self.subplots(1, len(cluster_ids))
+        for ax in axes.flat:
+            ax.plot(np.random.randn(100))
 
 
 def test_mpl_view(qtbot, tempdir):
@@ -32,9 +33,11 @@ def test_mpl_view(qtbot, tempdir):
     v.attach(gui)
     qtbot.addWidget(gui)
 
-    v.on_select(cluster_ids=[])
-
+    v.on_select(cluster_ids=[0])
     zoom_fun(v.axes[0, 0], Bunch(xdata=10, ydata=10, button='down'))
+
+    v.on_select(cluster_ids=[0, 1])
+    zoom_fun(v.axes[0, 1], Bunch(xdata=10, ydata=10, button='down'))
 
     # qtbot.stop()
     gui.close()
