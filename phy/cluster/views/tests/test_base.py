d4abeed9082776569dee8473a8a33f93a0432840
Cyrille Rossant <rossant@users.noreply.github.com>
2019-03-26 13:32:36 +0100

    Update base ManualClusteringView class

--- phy/cluster/views/tests/test_base.py
+++ phy/cluster/views/tests/test_base.py
@@ -8,10 +8,8 @@
 
 import numpy as np
 
-from phy.gui import GUI
-from phy.plot.visuals import ScatterVisual
-from phy.utils import Bunch
-from ..base import ManualClusteringView, ManualClusteringViewMatplotlib, zoom_fun
+from phy.utils._color import _colormap
+from ..base import ManualClusteringView
 
 
 #------------------------------------------------------------------------------
@@ -20,49 +18,16 @@ from ..base import ManualClusteringView, ManualClusteringViewMatplotlib, zoom_fu
 
 class MyView(ManualClusteringView):
     def on_select(self, cluster_ids, **kwargs):
-        v = ScatterVisual()
-        self.figure.add_visual(v)
-        v.set_data(pos=.25 * np.random.randn(100, 2))
+        for i in range(len(cluster_ids)):
+            self.canvas.scatter(pos=.25 * np.random.randn(100, 2), color=_colormap(i, 1))
 
 
-def test_manual_clustering_view_1(qtbot, tempdir):
+def test_manual_clustering_view_1(qtbot):
     v = MyView()
+    v.canvas.show()
 
-    gui = GUI(config_dir=tempdir)
-    gui.show()
-    v.attach(gui)
-    qtbot.addWidget(gui)
-    v.on_select(cluster_ids=[0])
-
-    # qtbot.stop()
-    gui.close()
-
-
-#------------------------------------------------------------------------------
-# Test matplotlib view
-#------------------------------------------------------------------------------
-
-class MyMatplotlibView(ManualClusteringViewMatplotlib):
-    def on_select(self, cluster_ids, **kwargs):
-        axes = self.subplots(1, len(cluster_ids))
-        for ax in axes.flat:
-            ax.plot(np.random.randn(100))
-
-
-def test_mpl_view(qtbot, tempdir):
-
-    v = MyMatplotlibView()
-
-    gui = GUI(config_dir=tempdir)
-    gui.show()
-    v.attach(gui)
-    qtbot.addWidget(gui)
-
-    v.on_select(cluster_ids=[0])
-    zoom_fun(v.axes[0, 0], Bunch(xdata=10, ydata=10, button='down'))
-
+    qtbot.addWidget(v.canvas)
     v.on_select(cluster_ids=[0, 1])
-    zoom_fun(v.axes[0, 1], Bunch(xdata=10, ydata=10, button='down'))
 
     # qtbot.stop()
-    gui.close()
+    v.canvas.close()
