77930d3200ba974de72a44ef2159a0d920d785e0
Cyrille Rossant <rossant@users.noreply.github.com>
2019-05-06 15:48:46 +0200

    Bug fixes related to busy state in supervisor

--- phy/apps/kwik/tests/test_gui.py
+++ phy/apps/kwik/tests/test_gui.py
@@ -17,7 +17,7 @@ from phy.cluster.views import WaveformView, TraceView
 from phy.gui.widgets import Barrier
 from phy.plot.tests import key_press
 from phy.utils import connect, reset
-from phy.utils.testing import captured_output, download_test_file, _in_travis
+from phy.utils.testing import captured_output, download_test_file
 from ..gui import KwikController, kwik_describe
 
 logger = logging.getLogger(__name__)
@@ -39,8 +39,6 @@ def controller(tempdir):
         shutil.copy(path, op.join(tempdir, op.basename(path)))
     kwik_path = op.join(tempdir, op.basename(paths[0]))
     c = KwikController(kwik_path)
-    if _in_travis():
-        c.supervisor._block_duration = 1000
     yield c
     # NOTE: make sure all callback functions are unconnected at the end of the tests
     # to avoid side-effects and spurious dependencies between tests.
