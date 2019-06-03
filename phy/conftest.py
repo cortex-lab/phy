33ad684b4b8efab81d82e575d97184dc004d0386
Cyrille Rossant <rossant@users.noreply.github.com>
2019-06-03 20:56:24 +0200

    Remove repeat option from coverage

--- phy/conftest.py
+++ phy/conftest.py
@@ -37,7 +37,8 @@ def pytest_addoption(parser):
     parser.addoption('--repeat', action='store', help='Number of times to repeat each test')
 
 
-def pytest_generate_tests(metafunc):
+def pytest_generate_tests(metafunc):  # pragma: no cover
+    # Use --repeat option.
     if metafunc.config.option.repeat is not None:
         count = int(metafunc.config.option.repeat)
         metafunc.fixturenames.append('tmp_ct')
