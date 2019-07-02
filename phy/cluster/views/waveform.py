Fix waveform scaling

phy/cluster/views/waveform.py
@@ -134,8 +134,8 @@ class WaveformView(ScalingMixin, ManualClusteringView):
         self.canvas.set_layout('boxed', box_bounds=[[-1, -1, +1, +1]])
         self.canvas.enable_axes()
 
-        self._box_scaling = np.ones(2)
-        self._probe_scaling = np.ones(2)
+        self._box_scaling = (1., 1.)
+        self._probe_scaling = (1., 1.)
 
         self.box_pos = np.array(self.canvas.boxed.box_pos)
         self.box_size = np.array(self.canvas.boxed.box_size)
@@ -197,16 +197,14 @@ class WaveformView(ScalingMixin, ManualClusteringView):
         # Find the x coordinates.
         t = get_linear_x(n_spikes_clu * n_channels, n_samples)
         t = _overlap_transform(t, offset=bunch.offset, n=bunch.n_clu, overlap=self.overlap)
-        # Get the spike masks.
-        m = masks
         # HACK: on the GPU, we get the actual masks with fract(masks)
         # since we add the relative cluster index. We need to ensure
         # that the masks is never 1.0, otherwise it is interpreted as
         # 0.
-        m *= .99999
+        masks *= .99999
         # NOTE: we add the cluster index which is used for the
         # computation of the depth on the GPU.
-        m += bunch.index
+        masks += bunch.index
 
         # Generate the box index (one number per channel).
         box_index = _index_of(channel_ids_loc, self.channel_ids)
@@ -219,7 +217,7 @@ class WaveformView(ScalingMixin, ManualClusteringView):
         wave = wave.reshape((n_spikes_clu * n_channels, n_samples))
 
         self.waveform_visual.add_batch_data(
-            x=t, y=wave, color=bunch.color, masks=m, box_index=box_index,
+            x=t, y=wave, color=bunch.color, masks=masks, box_index=box_index,
             data_bounds=self.data_bounds)
 
     def _plot_labels(self, channel_ids, n_clusters, channel_labels=None):
@@ -300,6 +298,11 @@ class WaveformView(ScalingMixin, ManualClusteringView):
         self.actions.add(self.shrink_vertically)
         self.actions.separator()
 
+    @property
+    def boxed(self):
+        """Layout instance."""
+        return self.canvas.boxed
+
     # Overlap
     # -------------------------------------------------------------------------
 
@@ -324,11 +327,6 @@ class WaveformView(ScalingMixin, ManualClusteringView):
         self.canvas.boxed.update_boxes(
             self.box_pos * self.probe_scaling, self.box_size)
 
-    @property
-    def boxed(self):
-        """Layout instance."""
-        return self.canvas.boxed
-
     def _apply_box_scaling(self):
         self.canvas.layout.scaling = self._box_scaling
 
@@ -340,24 +338,27 @@ class WaveformView(ScalingMixin, ManualClusteringView):
     @box_scaling.setter
     def box_scaling(self, value):
         assert len(value) == 2
-        self._box_scaling = np.array(value)
+        self._box_scaling = value
         self._apply_box_scaling()
 
     def widen(self):
         """Increase the horizontal scaling of the waveforms."""
-        self._box_scaling[0] *= self._scaling_param_increment
+        w, h = self._box_scaling
+        self._box_scaling = (w * self._scaling_param_increment, h)
         self._apply_box_scaling()
 
     def narrow(self):
         """Decrease the horizontal scaling of the waveforms."""
-        self._box_scaling[0] /= self._scaling_param_increment
+        w, h = self._box_scaling
+        self._box_scaling = (w / self._scaling_param_increment, h)
         self._apply_box_scaling()
 
     def _get_scaling_value(self):
-        return self.box_scaling[1]
+        return self._box_scaling[1]
 
     def _set_scaling_value(self, value):
-        self.box_scaling[1] = value
+        w, h = self._box_scaling
+        self.box_scaling = (w, value)
         self._update_boxes()
 
     # Probe scaling
@@ -371,27 +372,31 @@ class WaveformView(ScalingMixin, ManualClusteringView):
     @probe_scaling.setter
     def probe_scaling(self, value):
         assert len(value) == 2
-        self._probe_scaling = np.array(value)
+        self._probe_scaling = value
         self._update_boxes()
 
     def extend_horizontally(self):
         """Increase the horizontal scaling of the probe."""
-        self._probe_scaling[0] *= self._scaling_param_increment
+        w, h = self._probe_scaling
+        self._probe_scaling = (w * self._scaling_param_increment, h)
         self._update_boxes()
 
     def shrink_horizontally(self):
         """Decrease the horizontal scaling of the waveforms."""
-        self._probe_scaling[0] /= self._scaling_param_increment
+        w, h = self._probe_scaling
+        self._probe_scaling = (w / self._scaling_param_increment, h)
         self._update_boxes()
 
     def extend_vertically(self):
         """Increase the vertical scaling of the waveforms."""
-        self._probe_scaling[1] *= self._scaling_param_increment
+        w, h = self._probe_scaling
+        self._probe_scaling = (w, h * self._scaling_param_increment)
         self._update_boxes()
 
     def shrink_vertically(self):
         """Decrease the vertical scaling of the waveforms."""
-        self._probe_scaling[1] /= self._scaling_param_increment
+        w, h = self._probe_scaling
+        self._probe_scaling = (w, h / self._scaling_param_increment)
         self._update_boxes()
 
     # Navigation