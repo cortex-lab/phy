99247e6c3ffd68f147923700920f8691d6710548
Cyrille Rossant <rossant@users.noreply.github.com>
2019-07-03 15:01:10 +0200

    Export some phylib.io.model functions

--- phy/apps/template/__init__.py
+++ phy/apps/template/__init__.py
@@ -7,5 +7,5 @@
 # Imports
 #------------------------------------------------------------------------------
 
-from phylib.io.model import TemplateModel  # noqa
+from phylib.io.model import TemplateModel, from_sparse, get_template_params, load_model  # noqa
 from .gui import TemplateController, template_describe, template_gui  # noqa
