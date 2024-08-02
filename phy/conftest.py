601404cd93c383a23eaf1233f9b9810b552da83c
Zach McKenzie <92116279+zm711@users.noreply.github.com>
2024-08-02 15:39:18 -0400

    Can't filter warnings due to deprecations

--- phy/conftest.py
+++ phy/conftest.py
@@ -27,9 +27,9 @@ add_default_handler(5, logger=logger)
 # Fix the random seed in the tests.
 np.random.seed(2019)
 
-warnings.filterwarnings('ignore', category=matplotlib.cbook.mplDeprecation)
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+# warnings.filterwarnings('ignore', category=matplotlib.cbook.mplDeprecation)
+# warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+# warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 
 def pytest_addoption(parser):
