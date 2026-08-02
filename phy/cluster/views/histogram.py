Fix firing rate view normalization

phy/cluster/views/histogram.py
@@ -31,7 +31,7 @@ def _compute_histogram(
     assert x_min <= x_max
     assert n_bins >= 0
     n_bins = _clip(n_bins, 2, 1000000)
-    bins = np.linspace(float(x_min), float(x_max), int(n_bins))
+    bins = np.linspace(float(x_min), float(x_max), int(n_bins) + 1)
     if ignore_zeros:
         data = data[data != 0]
     histogram, _ = np.histogram(data, bins=bins)
@@ -151,6 +151,9 @@ class HistogramView(ScalingMixin, ManualClusteringView):
             box_index=bunch.index,
         )
 
+    def _compute_histogram(self, data):
+        return _compute_histogram(data, x_min=self.x_min, x_max=self.x_max, n_bins=self.n_bins)
+
     def get_clusters_data(self, load_all=None):
         bunchs = []
         for i, cluster_id in enumerate(self.cluster_ids):
@@ -167,9 +170,7 @@ class HistogramView(ScalingMixin, ManualClusteringView):
             assert self.x_min <= self.x_max
 
             # Compute the histogram.
-            bunch.histogram = _compute_histogram(
-                bunch.data, x_min=self.x_min, x_max=self.x_max, n_bins=self.n_bins
-            )
+            bunch.histogram = self._compute_histogram(bunch.data)
             bunch.ylim = bunch.histogram.max()
 
             bunch.color = selected_cluster_color(i)
@@ -414,3 +415,13 @@ class FiringRateView(HistogramView):
         f'set_x_min ({bin_unit})': f'{alias_char}min',
         f'set_x_max ({bin_unit})': f'{alias_char}max',
     }
+
+    def _compute_histogram(self, data):
+        counts = _compute_histogram(
+            data,
+            x_min=self.x_min,
+            x_max=self.x_max,
+            n_bins=self.n_bins,
+            normalize=False,
+        )
+        return counts / self.bin_size

phy/cluster/views/tests/test_histogram.py
@@ -7,12 +7,19 @@
 import numpy as np
 from phylib.utils import Bunch
 
-from ..histogram import FiringRateView, HistogramView, ISIView
+from ..histogram import FiringRateView, HistogramView, ISIView, _compute_histogram
 from . import _stop_and_close
 
 # ------------------------------------------------------------------------------
 # Test Histogram view
 # ------------------------------------------------------------------------------
+
+
+def test_compute_histogram_n_bins():
+    histogram = _compute_histogram(
+        np.array([0.5, 1.5, 2.5, 3.5]), x_min=0, x_max=4, n_bins=4, normalize=False
+    )
+    np.testing.assert_array_equal(histogram, np.ones(4))
 
 
 def test_histogram_view_0(qtbot, gui):
@@ -85,6 +92,23 @@ def test_firing_rate_view_ignores_global_x_max(qtbot, gui):
     v.on_select(cluster_ids=[0])
     assert v.x_max == 30.0
 
+    _stop_and_close(qtbot, v)
+
+
+def test_firing_rate_view_displays_spikes_per_second(qtbot):
+    v = FiringRateView(
+        cluster_stat=lambda cluster_id: Bunch(
+            data=np.arange(0.125, 2, 0.25),
+            x_min=0.0,
+            x_max=2.0,
+        )
+    )
+    v.n_bins = 2
+    v.cluster_ids = [0]
+
+    bunch = v.get_clusters_data()[0]
+
+    np.testing.assert_array_equal(bunch.histogram, np.array([4.0, 4.0]))
     _stop_and_close(qtbot, v)
 
 